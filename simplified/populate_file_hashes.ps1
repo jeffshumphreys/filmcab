b819a960441d35591224ffe639108b1f2b12690c
jeffshumphreys <jeffshumphreys@outlook.com>
2023-12-27 15:47:07 -0700

    actual used, fills in broken hashes

--- simplified/populate_file_hashes.ps1
+++ simplified/populate_file_hashes.ps1
@@ -1,10 +1,21 @@
-. .\include_filmcab_header.ps1
+<#
+    FilmCab Daily morning process: Calculate MD5 hashes for new files.
+    Status: Working in Test
+
+    This calculates MD5 hashes on all massive movie files.  This runs after all new files and directories have been mapped into the files table, all files verified as still existing.
+
+    So far MD5 hashes on contents have proven unique, though I don't know how long that will last. Should I use UUIDs?
+    One nice thing using the hash as a foreign key, if a file goes missing and then is reacquired, they'll line right up.
+    That and the hash will link up across any future tables.
+#>
+
+. .\simplified\includes\include_filmcab_header.ps1 # local to base directory of base folder for some reason.
 :doitallover while($true) {
 $DBReader = $DBConn.CreateCommand()
-$DBReader.CommandText = "SELECT txt AS file_path, id AS file_id, file_size FROM stage_for_master.files WHERE (file_lost IS NULL OR file_lost IS FALSE) AND (updated_file_hash IS NULL OR updated_file_hash IS FALSE)
-                        ORDER BY file_id";
+$DBReader.CommandText = "SELECT txt AS file_path, id AS file_id, file_size FROM stage_for_master.files WHERE file_lost IS DISTINCT FROM FALSE AND updated_file_hash IS DISTINCT FROM FALSE
+                        ORDER BY file_id"; # Helps with watching ids finish up in the database.
 $rtnrows = $DBReader.ExecuteReader()
-$jobMax        = 2
+$jobMax        = 2 # 2 results in 4 pwsh instances, 4 console window hosts. Visual Studio Code has 9 instances and I don't know why.  Will have to investigate. CPU bounces between 6 and 12%
 $jobs          = @()
 Get-Job|Stop-Job > $null
 Get-Job|Remove-Job > $null
@@ -22,16 +33,21 @@ $howmanyfilehashesupdated = 0
                 $file_id   = $Using:file_id
                 $file_size = $Using:file_size
 
+                $Conn                  = New-Object System.Data.Odbc.OdbcConnection;
+                $Conn.ConnectionString = $Using:connString;
+                $Conn.Open();
+                $Command               = $Conn.CreateCommand();
+
                 if (Test-Path -LiteralPath $file_path -PathType Leaf) {
                     $file_hash             = Get-FileHash -LiteralPath $file_path -Algorithm MD5;
-                    $Conn                  = New-Object System.Data.Odbc.OdbcConnection;
-                    $Conn.ConnectionString = "Driver={PostgreSQL UNICODE(x64)};Server=$Using:MyServer;Port=$Using:MyPort;Database=$Using:MyDB;Uid=$Using:MyUid;Pwd=$Using:MyPass;";
-                    $Conn.Open();
-                    $Command               = $Conn.CreateCommand();
                     $Command.CommandText   = "UPDATE stage_for_master.files SET file_md5_hash = '$($file_hash.Hash)'::bytea, updated_file_hash = True WHERE id = $file_id";
                     $Command.ExecuteNonQuery();
                     $Conn.Close();
-                } 
+                } else {
+                    $Command.CommandText   = "UPDATE stage_for_master.files SET file_lost = TRUE, file_loss_detected_on_ts_wth_tz = clock_timestamp() WHERE id = $file_id";
+                    $Command.ExecuteNonQuery();
+                    $Conn.Close();
+                }
             } 
             $job_id = $job.Id
             $jobs+= $job
@@ -71,16 +87,19 @@ $howmanyfilehashesupdated = 0
     }
 }
 $jobs | Wait-Job
-Receive-Job $jobs -Wait
-Get-Job
-Write-Host "How many file_ids corrected: $howmanyfilehashesupdated"
-
+if ($jobs.Count -gt 0) {
+    Receive-Job $jobs -Wait
+    Get-Job
+    Write-Host "How many file_ids corrected: $howmanyfilehashesupdated"
+}
 $DBReader = $DBConn.CreateCommand()
 $DBReader.CommandText = "SELECT COUNT(*) FROM stage_for_master.files WHERE (file_lost IS NULL OR file_lost IS FALSE) AND (updated_file_hash IS NULL OR updated_file_hash IS FALSE)";
 $rowsleft = $DBReader.ExecuteScalar()
-if ($rowsleft) {
+if ($rowsleft -gt 0) {
     Write-Host "Still rows!!!! $rowsleft"
     continue doitallllllover
+} else {
+    break doitallover
 }
 }
 Write-Host "Fine!"
