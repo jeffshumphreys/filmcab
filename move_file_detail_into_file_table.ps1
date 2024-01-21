3d8cc556485552f7edede53a015ae152d910e2be
jeffshumphreys <jeffshumphreys@outlook.com>
2024-01-20 18:37:02 -0700

    This c++ code is dead so can't run it

--- move_file_detail_into_file_table.ps1
+++
@@ -1,7 +0,0 @@
-<#
-REM https://doc.qt.io/Qt-5/windows-deployment.html
-REM worked. Added dlls to bin dir
-windeployqt --debug --verbose 2 D:\qt_projects\build-filmcab-Desktop_Qt_6_5_3_MinGW_64_bit-Debug\debug
-#>
-
-D:\qt_projects\build-filmcab-Desktop_Qt_6_5_3_MinGW_64_bit-Debug\debug\filmcab.exe -f "scan_folders_and_pull_file_details_into_database" -d filmcab --showprogress
