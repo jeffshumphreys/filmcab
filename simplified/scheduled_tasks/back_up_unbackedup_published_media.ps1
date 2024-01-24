5347f50fa85ccf732acde29e0983b2bc35c4ed6f
jeffshumphreys <jeffshumphreys@outlook.com>
2024-01-24 12:11:16 -0700

    Now targeting more clearly that I didn't generate this log.

--- simplified/scheduled_tasks/back_up_unbackedup_published_media.ps1
+++ simplified/scheduled_tasks/back_up_unbackedup_published_media.ps1
@@ -2,7 +2,7 @@
     Called from Windows Task Scheduler, Task is in \FilmCab, Task name is same as file
     Status: Run when run manually from Task Scheduler. The full paths were missing and so the log failed.
     Status: Update when runs at 11:52 PM tonight and generates log.
-
+#   https://github.com/jeffshumphreys/filmcab/tree/master/simplified
     Notes: I don't just love the verbose log output, but I'm only keeping the one copy.
     ###### Tue Jan 16 19:10:55 MST 2024 - Moved to Yet Another Subfolder. Updated actual task. Exported.
 
@@ -20,4 +20,4 @@ param()
 # /R:1 retry once instead of default 1 million
 # /XJ :: eXclude symbolic links (for both files and directories) and Junction points.  (Not doing this, just FYI)
 
-Robocopy.exe "O:\Video AllInOne" "G:\Video AllInOne Backup" /E /J /NOOFFLOAD /R:1 /V /UNILOG:D:\qt_projects\filmcab\simplified\_log\back_up_unbackedup_published_media.log
+Robocopy.exe "O:\Video AllInOne" "G:\Video AllInOne Backup" /E /J /NOOFFLOAD /R:1 /V /UNILOG:D:\qt_projects\filmcab\simplified\_log\back_up_unbackedup_published_media.robocopy.log
