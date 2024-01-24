b86720aa0f83ea70393b57155db813c4afe63619
jeffshumphreys <jeffshumphreys@outlook.com>
2024-01-23 19:11:26 -0700

    Getting my shit together: A common footer.

---
+++ simplified/_dot_include_standard_footer.ps1
@@ -0,0 +1,38 @@
+<#
+    Tied/Paired with _dot_include_standard_header.ps1. Won't work if header not included.
+#>                                                                                       
+
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '', Scope='Function', Target='Log-*')] # We don't need no stinkin' badges
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingCmdletAliases', '', Scope='Function', Target='*')] # Why?
+
+param()
+
+
+$elapsed = $scriptTimer.Elapsed # Tada!!
+
+Write-Host
+
+if ($elapsed.Days -gt 0) {
+    Format-Plural 'Day' $($elapsed.Days) -includeCount
+}
+elseif ($elapsed.Hours -gt 0) {
+    Format-Plural 'Hour' $($elapsed.Hours)
+}
+elseif ($elapsed.Minutes -gt 0) {
+    Format-Plural 'Minute' $($elapsed.Minutes)
+}
+elseif ($elapsed.Seconds -gt 0) {
+    Format-Plural 'Second' $($elapsed.Seconds)
+}
+elseif ($elapsed.Milliseconds -gt 0) {
+    Format-Plural 'Millisecond' $($elapsed.Milliseconds) -includeCount
+}
+elseif ($elapsed.Microseconds -gt 0) {
+    Format-Plural 'Microsecond' $($elapsed.Microseconds)
+}
+elseif ($elapsed.Ticks -gt 0) {
+    Format-Plural 'Tick' $($elapsed.Ticks)
+}
+
+# Log-ScriptCompleted
+
