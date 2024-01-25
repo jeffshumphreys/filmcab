424feea079c34cacb2f0214f066883be3ef65c3d
jeffshumphreys <jeffshumphreys@outlook.com>
2024-01-25 13:31:07 -0700

    Adding more pulled props for tasks.

--- simplified/pull_scheduled_task_definitions.ps1
+++ simplified/pull_scheduled_task_definitions.ps1
@@ -42,12 +42,16 @@ foreach ($scheduledTaskDefPath in $scheduledTaskDefPaths) {
         task_principal_group_id = (@($taskXML.Task.Principals.Principal.PSObject.Properties.Name -eq 'GroupId').Count -eq 1 ? $taskXML.Task.Principals.Principal.GroupId : '')
         task_principal_logon_type = (@($taskXML.Task.Principals.Principal.PSObject.Properties.Name -eq 'LogonType').Count -eq 1 ? $taskXML.Task.Principals.Principal.LogonType : '')
         task_principal_run_level = (@($taskXML.Task.Principals.Principal.PSObject.Properties.Name -eq 'RunLevel').Count -eq 1 ? $taskXML.Task.Principals.Principal.RunLevel : '')
+        # ProcessTokenSidType
+        # DisplayName
+        # RequiredPrivileges
         MultipleInstancesPolicy        = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'MultipleInstancesPolicy'        ).Count -eq 1 ? $taskXML.Task.Settings.MultipleInstancesPolicy             : '')
         DisallowStartIfOnBatteries     = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'DisallowStartIfOnBatteries').Count -eq 1 ? $taskXML.Task.Settings.DisallowStartIfOnBatteries          : '')
         StopIfGoingOnBatteries         = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'StopIfGoingOnBatteries').Count -eq 1 ? $taskXML.Task.Settings.StopIfGoingOnBatteries              : '')
         AllowHardTerminate             = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'AllowHardTerminate').Count -eq 1 ? $taskXML.Task.Settings.AllowHardTerminate                  : '')
         StartWhenAvailable             = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'StartWhenAvailable').Count -eq 1 ? $taskXML.Task.Settings.StartWhenAvailable                  : '')
         RunOnlyIfNetworkAvailable      = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'RunOnlyIfNetworkAvailable').Count -eq 1 ? $taskXML.Task.Settings.RunOnlyIfNetworkAvailable           : '')
+        # NetworkProfileName
         StopOnIdleEnd                   = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'IdleSettings').Count -eq 1 ? $taskXML.Task.Settings.IdleSettings.StopOnIdleEnd                        : '')
         RestartOnIdle                   = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'IdleSettings').Count -eq 1 ? $taskXML.Task.Settings.IdleSettings.RestartOnIdle                        : '')
         AllowStartOnDemand             = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'AllowStartOnDemand').Count -eq 1 ? $taskXML.Task.Settings.AllowStartOnDemand                  : '')
@@ -59,6 +63,7 @@ foreach ($scheduledTaskDefPath in $scheduledTaskDefPaths) {
         WakeToRun                      = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'WakeToRun').Count -eq 1 ? $taskXML.Task.Settings.WakeToRun                           : '')
         ExecutionTimeLimit             = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'ExecutionTimeLimit').Count -eq 1 ? $taskXML.Task.Settings.ExecutionTimeLimit                  : '')
         Priority                       = (@($taskXML.Task.Settings.PSObject.Properties.Name -eq 'Priority').Count -eq 1 ? $taskXML.Task.Settings.Priority                            : '')
+        # DeleteExpiredTaskAfter
 
     }    
     $taskDefs+= $taskDef
@@ -80,6 +85,7 @@ foreach ($scheduledTaskDefPath in $scheduledTaskDefPaths) {
             $actionDef.Arguments = $taskAction.Exec.Arguments
             $actionDef.WorkingDirectory = (@($taskAction.Exec.PSObject.Properties.Name -eq 'WorkingDirectory').Count -eq 1 ? $taskAction.Exec.WorkingDirectory : '')
         }
+        # ComHandler (ClassId, Data)
         $actionDefs+= $actionDef
     }                       
     
@@ -89,15 +95,53 @@ foreach ($scheduledTaskDefPath in $scheduledTaskDefPaths) {
         $triggerType = (@($taskTrigger.PSObject.Properties.Name -eq 'CalendarTrigger').Count -eq 1 ? 'Calendar': '?')  # Registration, Boot, Idle, Time, Event, Logon, SessionStateChange
         $triggerDef        = [PSCustomObject]@{
             task_full_path = $taskXML.Task.RegistrationInfo.URI
+            trigger_type = ''
+            Enabled        = ''
             StartBoundary = [Datetime]0
             EndBoundary = [Datetime]0
             Repetition = ''
             ExecutionTimeLimit = ''
+            Interval           = ''
+            Duration= ''
+            StopAtDurationEnd= ''
+            Delay= ''
+            RandomDelay= ''
+            Subscription= ''
+            PeriodOfOccurrence= ''
+            DaysInterval= ''
         }                                                 
 
+        $triggerDef.trigger_type = $triggerType
+        
         if ($triggerType -eq 'Calendar') {
-            $triggerDef.StartBoundary = $taskTrigger.CalendarTrigger.StartBoundary
+            $triggerDef.Enabled = (@($taskTrigger.CalendarTrigger.PSObject.Properties.Name -eq 'Enabled').Count -eq 1 ? $taskTrigger.CalendarTrigger.Enabled : '')
+            $triggerDef.StartBoundary = (@($taskTrigger.CalendarTrigger.PSObject.Properties.Name -eq 'StartBoundary').Count -eq 1 ? $taskTrigger.CalendarTrigger.StartBoundary : '')
             $triggerDef.EndBoundary = (@($taskTrigger.CalendarTrigger.PSObject.Properties.Name -eq 'EndBoundary').Count -eq 1 ? $taskTrigger.CalendarTrigger.EndBoundary : '')
+            $triggerDef.ExecutionTimeLimit = (@($taskTrigger.CalendarTrigger.PSObject.Properties.Name -eq 'ExecutionTimeLimit').Count -eq 1 ? $taskTrigger.CalendarTrigger.ExecutionTimeLimit : '')
+            # RandomDelay
+            if (@($taskTrigger.CalendarTrigger.PSObject.Properties.Name -eq 'Repetition').Count -eq 1) {
+                Fill-Property $triggerDef $taskTrigger.CalendarTrigger.Repetition 'Duration'
+                $triggerDef.Interval = (@($taskTrigger.CalendarTrigger.Repetition.PSObject.Properties.Name -eq 'Interval').Count -eq 1 ? $taskTrigger.CalendarTrigger.Repetition.Interval : '')
+                $triggerDef.Duration = (@($taskTrigger.CalendarTrigger.Repetition.PSObject.Properties.Name -eq 'Duration').Count -eq 1 ? $taskTrigger.CalendarTrigger.Repetition.Duration : '')
+                $triggerDef.StopAtDurationEnd = (@($taskTrigger.CalendarTrigger.Repetition.PSObject.Properties.Name -eq 'StopAtDurationEnd').Count -eq 1 ? $taskTrigger.CalendarTrigger.Repetition.StopAtDurationEnd : '')
+                
+            }                                                                                           
+            if (@($taskTrigger.CalendarTrigger.PSObject.Properties.Name -eq 'ScheduleByDay').Count -eq 1) {
+                #DaysInterval (integer)
+            }         
+            # WeeksInterval
+            # DaysOfWeek
+            # DaysOfMonth
+            # Months
+            # Weeks
+        # logonTrigger
+        # eventTrigger (MatchingElement, ValueQueries)
+        # RegistrationTrigger
+        # TimeTrigger
+        # bootTrigger
+        # Session State (ConsolConnect/Disconnect, RemoteConnect/SessionLock)
+
+
         }
         $triggerDefs+= $triggerDef
     }
