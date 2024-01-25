2c24da3781a2aac6660124ae71daca72e5a81f43
jeffshumphreys <jeffshumphreys@outlook.com>
2024-01-25 14:58:01 -0700

    Moved out of main folder; probably won't use.

--- needs_work/convert_sched_tasks_xml_to_rolledup_class_array.ps1
+++ needs_work/convert_sched_tasks_xml_to_rolledup_class_array.ps1
@@ -4,36 +4,171 @@
 
 ####################################################################################################################################################################>
 
-$path_to_extracted_task_xml = "D:\qt_projects\filmcab\all_sched_tasks.xml"
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '', Scope='Function', Target='Log-*', Justification='Log is a verb. It shortens my code not using Write-Log etc.')]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingCmdletAliases', '')]
+param()
 
-if (-Not (Test-Path $path_to_extracted_task_xml -PathType Leaf)) { 
+$path_to_extracted_task_xml = "D:\qt_projects\filmcab\simplified\all_sched_tasks.xml"
+
+$taskobjects = @()
+$taskactions = @()
+$tasktriggers = @()
+
+<#
+    if ((Test-Path $path_to_extracted_task_xml -PathType Leaf)) { 
+        # Get file timestamp
+        # Get last task registration event timestamps
+        if > than file time
+            reload entirety?
+            Or just load those entries added. But, the index will not match. Maybe just add to end?  Hmmm
+        else
+            use file, no reload
+#>
+
+if ($true) { 
     $sw = [Diagnostics.Stopwatch]::StartNew()
     $ErrorActionPreference = 'Stop' # Mandatory for Catch to work
+    $registeredtaskobjects = Get-ScheduledTask|Select *
     $registeredtasksxml = @()
-            $howmanyregisteredtasks = (Get-ScheduledTask).Count
-            for (($i = 0); $i -lt $howmanyregisteredtasks; $i++) {
-            #for (($i = 0); $i -lt 10; $i++) {
-                $i
-                try {
-                    $registeredtaskxml = [XML]((Get-ScheduledTask)[$i] | Export-ScheduledTask)
-                    $registeredtasksxml+= [pscustomobject]@{id=$i; xmldata = $registeredtaskxml}
-                    }
-                    catch {
-                        Write-Host "Error on #$i"
-                        $blockedExport = (Get-ScheduledTask)[$i]
-                        $blockedTaskPath = $blockedExport.TaskPath
-                        $blockedTaskName =$blockedExport.TaskName
-                        $blockedtaskxml = [XML](Export-ScheduledTask -TaskName "$blockedTaskName" -TaskPath "$blockedTaskPath")
-                        $registeredtasksxml+= [pscustomobject]@{id=$i; xmldata = $blockedtaskxml}
-                    }
+    $howmanyregisteredtasks = $registeredtaskobjects.Count
+    #$howmanyregisteredtasks = 1
+    $startatindex = 0
+    for (($task_index = $startatindex); $task_index -lt $howmanyregisteredtasks; $task_index++) {
+        $registeredtaskobject = [PSCustomObject]@{
+            task_id                              = [int]-1;
+            task_path                            = '';
+            task_name                            = '';
+            task_full_path                       = '';
+            task_description                     = '';
+            task_author                          = '';
+            task_state                           = '';
+            task_source                          = '';
+            task_version                         = '';
+            task_creation_date                   = [datetime]0;
+            task_multi_inst_run_rule             = ''; #MultipleInstances = '';  #IgnoreNew, Parallel
+            execution_time_limit                 = '';  #PT72H
+            security_descriptor                  = '';
+            compatibility                        = '';  #Vista
+            delete_expired_task_after            = ''; #
+            task_is_enabled                      = [bool]$null;  #True
+            task_is_hidden                       = [bool]$null;  #False
+            allow_demand_start                   = [bool]$null;
+            allow_hard_terminate                 = [bool]$null;  #True
+            disallow_start_if_on_batteries       = [bool]$null;  #True
+            priority                             = [int]-1;  #7,           4
+            restart_count                        = [int]-1;  #0,           2
+            restart_interval                     = ''; #
+            run_only_if_idle                     = [bool]$null;  #False
+            run_only_if_network_available        = [bool]$null;  #False
+            start_when_available                 = [bool]$null;  #True
+            stop_if_going_on_batteries           = [bool]$null;  #True
+            wake_to_run                          = [bool]$null;  #False
+            disallow_start_on_remote_app_session = [bool]$null;  #False
+            use_unified_scheduling_engine        = [bool]$null;  #False,   True
+            volatile                             = [bool]$null;  #False
+            restart_on_idle                      = [bool]$null;
+            stop_on_idle_end                     = [bool]$null;
+            task_computer                        = ''; #PSComputerName    = '';  #
+            network_id                           = '';
+            network_name                         = '';
+            network_computer                     = '';
+            idle_duration                        = ''; #PT10M
+            idle_wait_timeout                    = ''; # PT1H
+            idle_computer                        = ''; #PSComputerName
+            maintenance_deadline                 = ''; # P2D
+            maintenance_run_exclusive                = [bool]$null; #False
+            maintenance_period                   = ''; # P1D
+            maintenance_computer                 = '' #PSComputerName
+            principal_id                         = ''
+            principal_display_name               = ''
+            user_id                              = ''
+            required_privileges                  = [string[]] {''}
+            logon_type                           = ''
+            group_id                             = ''
+            run_level                            = ''
+            process_token_sid_type               = ''
+            principal_computer                   = ''
+            task_xml                             = [XML]$null;
+        }
+        try {
+            $registeredtaskobjectraw = $registeredtaskobjects[$task_index]
+            $registeredtaskobject.task_id                              = $task_index
+            $registeredtaskobject.task_path                            = $registeredtaskobjectraw.TaskPath
+            $registeredtaskobject.task_name                            = $registeredtaskobjectraw.TaskName
+            $registeredtaskobject.task_full_path                       = $registeredtaskobjectraw.URI
+            $registeredtaskobject.task_creation_date                   = $registeredtaskobjectraw.Date
+            $registeredtaskobject.task_description                     = $registeredtaskobjectraw.Description
+            $registeredtaskobject.task_state                           = $registeredtaskobjectraw.State
+            $registeredtaskobject.task_source                          = $registeredtaskobjectraw.Source
+            $registeredtaskobject.task_author                          = $registeredtaskobjectraw.Author
+            $registeredtaskobject.task_version                         = $registeredtaskobjectraw.Version
+            $registeredtaskobject.task_is_enabled                      = $registeredtaskobjectraw.Settings.Enabled
+            $registeredtaskobject.task_is_hidden                       = $registeredtaskobjectraw.Settings.Hidden
+            $registeredtaskobject.task_multi_inst_run_rule             = $registeredtaskobjectraw.Settings.MultipleInstances
+            $registeredtaskobject.task_computer                        = $registeredtaskobjectraw.PSComputerName
+            $registeredtaskobject.execution_time_limit                 = $registeredtaskobjectraw.Settings.ExecutionTimeLimit
+            $registeredtaskobject.security_descriptor                  = $registeredtaskobjectraw.SecurityDescriptor
+            $registeredtaskobject.allow_demand_start                   = $registeredtaskobjectraw.Settings.AllowDemandStart
+            $registeredtaskobject.allow_hard_terminate                 = $registeredtaskobjectraw.Settings.AllowHardTerminate
+            $registeredtaskobject.compatibility                        = $registeredtaskobjectraw.Settings.Compatibility
+            $registeredtaskobject.delete_expired_task_after            = $registeredtaskobjectraw.Settings.DeleteExpiredTaskAfter
+            $registeredtaskobject.disallow_start_if_on_batteries       = $registeredtaskobjectraw.Settings.DisallowStartIfOnBatteries
+            $registeredtaskobject.disallow_start_on_remote_app_session = $registeredtaskobjectraw.Settings.DisallowStartOnRemoteAppSession
+            $registeredtaskobject.priority                             = $registeredtaskobjectraw.Settings.Priority
+            $registeredtaskobject.restart_count                        = $registeredtaskobjectraw.Settings.RestartCount
+            $registeredtaskobject.restart_interval                     = $registeredtaskobjectraw.Settings.RestartInterval
+            $registeredtaskobject.run_only_if_idle                     = $registeredtaskobjectraw.Settings.RunOnlyIfIdle
+            $registeredtaskobject.run_only_if_network_available        = $registeredtaskobjectraw.Settings.RunOnlyIfNetworkAvailable
+            $registeredtaskobject.start_when_available                 = $registeredtaskobjectraw.Settings.StartWhenAvailable
+            $registeredtaskobject.stop_if_going_on_batteries           = $registeredtaskobjectraw.Settings.StopIfGoingOnBatteries
+            $registeredtaskobject.wake_to_run                          = $registeredtaskobjectraw.Settings.WakeToRun
+            $registeredtaskobject.use_unified_scheduling_engine        = $registeredtaskobjectraw.Settings.UseUnifiedSchedulingEngine
+            $registeredtaskobject.principal_id                         = $registeredtaskobjectraw.Principal.Id
+            $registeredtaskobject.principal_display_name               = $registeredtaskobjectraw.Principal.DisplayName
+            $registeredtaskobject.group_id                             = $registeredtaskobjectraw.Principal.GroupId
+            $registeredtaskobject.user_id                              = $registeredtaskobjectraw.Principal.UserId
+            $registeredtaskobject.principal_computer                   = $registeredtaskobjectraw.Principal.PSComputerName
+            $registeredtaskobject.required_privileges                  = $registeredtaskobjectraw.Principal.RequiredPrivilege #array
+            $registeredtaskobject.logon_type                           = $registeredtaskobjectraw.Principal.LogonType
+            $registeredtaskobject.process_token_sid_type               = $registeredtaskobjectraw.Principal.ProcessTokenSidType
+            $registeredtaskobject.run_level                            = $registeredtaskobjectraw.Principal.RunLevel            
+            
+            if (@($registeredtaskobjectraw.Settings.psobject.properties|Where Name -eq "NetworkSettings").Count -eq 1) {
+                $registeredtaskobject.network_id         = $registeredtaskobjectraw.Settings.NetworkSettings.Id
+                $registeredtaskobject.network_name       = $registeredtaskobjectraw.Settings.NetworkSettings.Name
+                $registeredtaskobject.network_computer   = $registeredtaskobjectraw.Settings.NetworkSettings.PSComputerName
             }
+            if (@($registeredtaskobjectraw.Settings.psobject.properties|Where Name -eq "IdleSettings").Count -eq 1) {
+                $registeredtaskobject.idle_duration      = $registeredtaskobjectraw.Settings.IdleSettings.IdleDuration
+                $registeredtaskobject.restart_on_idle    = $registeredtaskobjectraw.Settings.IdleSettings.RestartOnIdle
+                $registeredtaskobject.stop_on_idle_end   = $registeredtaskobjectraw.Settings.IdleSettings.StopOnIdleEnd            
+                $registeredtaskobject.idle_computer      = $registeredtaskobjectraw.Settings.IdleSettings.PSComputerName
+            }
+            if (@($registeredtaskobjectraw.Settings.psobject.properties|Where Name -eq "MaintenanceSettings").Count -eq 1) {
+                if ($null -ne $registeredtaskobjectraw.Settings.MaintenanceSettings) {
+                    $registeredtaskobject.maintenance_deadline      = $registeredtaskobjectraw.Settings.MaintenanceSettings.Deadline
+                    $registeredtaskobject.maintenance_run_exclusive = $registeredtaskobjectraw.Settings.MaintenanceSettings.Exclusive
+                    $registeredtaskobject.maintenance_period        = $registeredtaskobjectraw.Settings.MaintenanceSettings.Period
+                    $registeredtaskobject.maintenance_computer      = $registeredtaskobjectraw.Settings.MaintenanceSettings.PSComputerName
+                }
+            }              
+            
+            # Actions
+            # Triggers
+            $registeredtaskobject.task_xml               = [XML](Export-ScheduledTask -TaskPath $registeredtaskobject.task_path -TaskName $registeredtaskobject.task_name)
+        }
+        catch {
+            Write-Host "Error on #$task_index"
+        }
+        $taskobjects+= $registeredtaskobject
+    }
     $sw.Stop()
     $sw.Elapsed
 
-    $registeredtasksxml|Export-Clixml -Path $path_to_extracted_task_xml
+    $taskobjects|Export-Clixml -Path $path_to_extracted_task_xml
 }
 
-
+exit
 <####################################################################################################################################################################
 
      Load previously extracted registered scheduled tasks.
@@ -41,55 +176,6 @@ if (-Not (Test-Path $path_to_extracted_task_xml -PathType Leaf)) {
 ###################################################################################################################################################################>
 
 $registeredtasksxml = Import-Clixml -Path $path_to_extracted_task_xml
-
-# Parse the XML hierarchy into 3 arrays: The Tasks, their Actions, and their Triggers. Principals are part of the task since they appear to be one-to-one.
-
-$tasksExpanded = 
-$registeredtasksxml|
-    Select @{Name ='task_id';expression = {$_.id}} -Expand xmldata|
-    Select task_id -Expand Task
-
-$taskRegistrationInfo = 
-$tasksExpanded|
-    Select task_id, 
-        @{Name ='task_full_name'          ; Expression = {$_.URI}},
-        @{Name ='task_creation_date'      ; Expression = {$_.Date}},
-        @{Name ='task_author'             ; Expression = {$_.Author}},
-        @{Name ='task_description'        ; Expression = {$_.Description}},
-        @{Name ='task_security_descriptor'; Expression = {$_.SecurityDescriptor}},
-        @{Name ='task_source'             ; Expression = {$_.Source}}
-
-$tasksSettings =
-$tasksExpanded|
-    Select task_id -Expand Settings|
-    Select task_id, 
-        @{Name ='MultipleInstancesPolicy'        ; Expression = {$_.MultipleInstancesPolicy}},# : IgnoreNew
-        @{Name ='DisallowStartIfOnBatteries'     ; Expression = {$_.DisallowStartIfOnBatteries}},# : false
-        @{Name ='StopIfGoingOnBatteries'         ; Expression = {$_.StopIfGoingOnBatteries}},# : false
-        @{Name ='AllowHardTerminate'             ; Expression = {$_.URI}},# : true
-        @{Name ='StartWhenAvailable'             ; Expression = {$_.URI}},# : true
-        @{Name ='RunOnlyIfNetworkAvailable'      ; Expression = {$_.URI}},# : false
-        @{Name ='IdleSettings'                   ; Expression = {$_.URI}},# : IdleSettings
-        @{Name ='AllowStartOnDemand'             ; Expression = {$_.URI}},# : true
-        @{Name ='Enabled'                        ; Expression = {$_.URI}},# : true
-        @{Name ='Hidden'                         ; Expression = {$_.URI}},# : false
-        @{Name ='RunOnlyIfIdle'                  ; Expression = {$_.URI}},# : false
-        @{Name ='DisallowStartOnRemoteAppSession'; Expression = {$_.URI}},# : false
-        @{Name ='UseUnifiedSchedulingEngine'     ; Expression = {$_.URI}},# : false
-        @{Name ='WakeToRun'                      ; Expression = {$_.URI}},# : false
-        @{Name ='ExecutionTimeLimit'             ; Expression = {$_.URI}},# : PT12H5M
-        @{Name ='Priority'                       ; Expression = {$_.URI}}# : 7
-
-$taskPrincipals = 
-$tasksExpanded|
-    Select task_id -Expand Principals|
-    Select task_id -Expand Principal|
-    Select task_id, 
-        @{Name ='principal_id';Expression={$_.id}}, 
-        @{Name ='user_id'     ; Expression= {$_.UserId}}, 
-        @{Name ='logon_type'  ; Expression= {$_.LogonType}}, 
-        @{Name ='group_id'    ; Expression = {$_.GroupId}}, 
-        @{Name ='run_level'   ; Expression = {$_.RunLevel}}
 
 <#
     Most Actions are of type Exec, the rest are two separate paired objects: Context and ComHandle, not something I expect will trigger, so I'm not loading them.
@@ -110,12 +196,12 @@ $registeredtasksxml|
         }| Where action_command_path -ne $null
 
 $taskActionsExec = 
-$registeredtasksxml|ForEach-Object {$index = 0} {
-    Select @{Name ='task_id';expression = {$_.id}} -Expand xmldata|Select task_id -Expand Task|
-    {
-        $_.task_id
-    }
-    }
+# $registeredtasksxml|ForEach-Object {$index = 0} {
+#     Select @{Name ='task_id';expression = {$_.id}} -Expand xmldata|Select task_id -Expand Task|
+#     {
+#         $_.task_id
+#     }
+#     }
 
 $taskCalendarTriggers = 
 $registeredtasksxml| ForEach-Object {
@@ -157,7 +243,7 @@ Class TaskFlatDef {
     [string]   $action_command_arguments      = ''
     [string]   $action_command_working_dir    = ''
 
-    TaskFlatDef([pscustomobject]$ob) {
+    TaskFlatDef([PSCustomObject]$ob) {
         $ob.psobject.properties | Foreach { 
             $type = $_.TypeNameOfValue
             #Write-Host "input type = $type"
@@ -170,18 +256,16 @@ Class TaskFlatDef {
             }
         }
     }
-    InitFromPSCustOb([pscustomobject]$ob) {
+    InitFromPSCustOb([PSCustomObject]$ob) {
         $ob.psobject.properties | Foreach { 
-            $type = $_.TypeNameOfValue
-            #Write-Host "input type = $type for target $($_.Name)"
             $targettype = "String"
 
             if ($null -ne ($this.($_.Name))) {
                 $targettype = ($this.($_.Name)).GetType().Name
             }
-            #Write-Host "target type = $targettype"
+
             if ($targettype -eq 'datetime' -and $null -eq $_.Value) {
-                $this.($_.Name) = 0
+                $this.($_.Name) = 0 # Cannot assign a $null to a datetime
             } else {
                 $this.($_.Name) = $_.Value
             }
@@ -204,7 +288,7 @@ Class TaskActionExecDef {
     [string] $action_command_path  = ''
     [string] $action_command_arguments = ''
     [string] $action_command_working_dir = ''
-    TaskActionExecDef([pscustomobject]$ob) {
+    TaskActionExecDef([PSCustomObject]$ob) {
         $ob.psobject.properties | Foreach { 
             $type = $_.TypeNameOfValue
             #Write-Host "input type = $type"
@@ -229,7 +313,7 @@ Class TaskTriggerCalFlatDef {
     [string]   $daysinterval                  = ''
     [string]   $weeksinterval                 = ''
     [string]   $daysofweek                    = ''
-    TaskTriggerCalFlatDef([pscustomobject]$ob) {
+    TaskTriggerCalFlatDef([PSCustomObject]$ob) {
         $ob.psobject.properties | Foreach { 
             $type = $_.TypeNameOfValue
             #Write-Host "input type = $type"
