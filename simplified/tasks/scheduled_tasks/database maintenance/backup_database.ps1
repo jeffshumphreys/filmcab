6cf0e6397c19e05ff6616d27bdea724d46b5e926
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-04 13:08:08 -0700

    Stripped out the separate ddl task since the dates need to be the same and they need to use the same parameters.

--- simplified/tasks/scheduled_tasks/database maintenance/backup_database.ps1
+++ simplified/tasks/scheduled_tasks/database maintenance/backup_database.ps1
@@ -13,6 +13,22 @@
  
 . D:\qt_projects\filmcab\simplified\shared_code\_dot_include_standard_header.ps1
 
-'pg_dump.exe --verbose --host=localhost --port=5432 ****** --format=c --file "C:\filmcab backups/dump-filmcab-database.sql" -n "simplified" filmcab'
+$RidiculousLongTimestamp = (Get-Date).DateTime -replace ':', ' '
+
+# format c = Output a custom-format archive suitable for input into pg_restore. Together with the directory output format, this is the most flexible output format in that it allows manual selection and reordering of archived items during restore. This format is also compressed by default
+                                                                      
+$file_name_base = "C:\FilmCab Backups/dump-filmcab-database-data_and_schema-.$RidiculousLongTimestamp"
+
+pg_dump.exe --verbose --format=c --file "$file_name_base-compressed.sql" --dbname=filmcab --schema=simplified --blobs
+                                                                                                                                                  
+# format p = simple text
+
+pg_dump.exe --verbose --format=p --file "$file_name_base-in_text.sql" --dbname=filmcab --schema=simplified --blobs
+
+pg_dump.exe --verbose --format=p --file "$file_name_base-table-files-in_text.sql" --dbname=filmcab --schema=simplified --table=files --blobs
+
+$file_name_base = "C:\FilmCab Backups/dump-filmcab-database-schema-only-.$RidiculousLongTimestamp"
+                                                                       
+pg_dump.exe --verbose --format=p --file "$file_name_base-in_text.sql" --schema-only --dbname=filmcab --schema=simplified --blobs
 
 . D:\qt_projects\filmcab\simplified\shared_code\_dot_include_standard_footer.ps1
