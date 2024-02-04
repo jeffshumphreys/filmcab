d0a14d4be66b81ab79b4f941c756fa21a079f509
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-04 13:27:32 -0700

    Spread out icons, more in use.

--- simplified/shared_code/_dot_include_standard_header.ps1
+++ simplified/shared_code/_dot_include_standard_header.ps1
@@ -13,32 +13,58 @@
        
     A pile of possible helpful icons for output.
     🚨🛑🚚🏗⚓🧨🎉🎯🦺👑💡📼📀📒🔖💰💳📧📬✏📅📉📊📌📏⛏🛠🔧🔗⛓️‍💥⛓🧰💊🚪🚫🆕🆗🔴🔘🚩🏁🎌🏴‍☠️🏓❄🌪🚜🚑🐣🏃‍♂️👷🕵️‍♂️🙋‍♂️👈⚠🗯♻✂☢↩♾✳🌡☁🕹🎟🖥🎞🏷🗃🗄🗑🗝⚙⚰
-    ⛔, ❌, ❎  => Delete.
+    ⛔, ❌, ❎  => Delete.                                   
+
     ⭐, 🌟 => Added, inserted. Not great. 
+
     📝, ✍ => Update or edit. No good update I can find. Suggested 🔄, but more like a Refresh. 🔨?
-    🥂, 👏 => Success, in terms of a feature completed
+
+    🥂, 👏 => Success, in terms of a feature completed.
+
     💣 => Crashed. Not a captured fail. Also, 💥 which is collision, maybe a deadlock? Race condition?
-    🙁 => Failed opposite of success, but not a crash
-    🤨 => Warning, concerned. Why'd we get that value?
+
+    🙁 => Failed, opposite of success, but not a crash. Maybe the network wasn't available - and we caught it. Uncaught errors are probably crashes
+
+    🤨 => Warning, concerned. Why'd we get that value?  
+    
+    💡  => I have an idea, a concept, it's barely conceived.
 
     🚧 => Working on. Still good after 30 years.
+
     👀 => Detected an event or error.                                                            
+
     🔍 => Search for or a field allowing a user to enter search values.
+
     📈, ✅, 👌, 👍, ✔.  => Verified correct.
+
     🏭 => Factory
+
     🌙 => "This process runs at night"
+
     🧹 => clean up after.
+
     📦 => A package that is to be delivered or made available for download.
+
     📃 => Document, not necessarily a text document.       
+
     🧪 => Test. 🎓 Passed all tests. Deployable.                                                                                              
+
     ⏰=> Scheduled event or task, triggers an execution
+
     ⚡ => Execute, start a process. Probably should be different between blocking and async.
+
     ⏳ => Some unwanted but perhaps unavoidable delay. Long running process. OR, waiting for when more stuff has been written, then write this.
+
     🐌 => Running reallly slow!
+
     🌱 => Seed (as in, torrent?) Creation? 
-    🦄 => Very unlikely
-    🐷 => Resource hog
+
+    🦄 => Very unlikely to happen
+
+    🐷 => Resource hog           
+
     🧙 => Wizard, no sure what exactly to apply to
+    
     🔒 => Is locked, or does lock
     💀 => This code or path or branch is dead, not being worked on
     💩 => This code or program or module is crap.
@@ -47,12 +73,18 @@
     🐛 => bug!     
     👓 => scan or flag for scanning
     👴 => Code is showing its age
-    💤, 😴 => Process is sleeping
-    🏋️‍♂️ => Portion of code doing the majority of the work
+    💤, 😴 => Process is sleeping, or development has stagnated
+
+    🏋️‍♂️ => Portion of code doing the majority of the work  
+
     🎲 => Random output, or quasi-random, or intermittent failure
-    🧮 => Counts?
-    🌊 => Flood of data, tsunami of input or output
+
+    🧮 => Counts? Database? Collection?
+
+    🌊 => Flood of data, tsunami of input or output. Flood of work.
+
     🪓=> This needs refactoring
+    
     🔁 => Try op again
     
 #>                                                                                                
