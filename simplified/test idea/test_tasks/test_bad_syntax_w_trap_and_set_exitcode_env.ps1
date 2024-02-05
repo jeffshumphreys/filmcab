fdc7d7b6712ef329e0b955e59d9cde16ae62ed0f
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-05 16:40:03 -0700

    Finally. Success.

---
+++ simplified/test idea/test_tasks/test_bad_syntax_w_trap_and_set_exitcode_env.ps1
@@ -0,0 +1,20 @@
+
+Add-Type -name user32 -namespace win32 -memberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
+$consoleHandle = (get-process -id $pid).mainWindowHandle
+[void][win32.user32]::showWindow($consoleHandle, 0)
+
+############## Environment things FORCED on the user of this dot file.
+
+# Stop on an error, please.  Lazy shits at City of Boise prefer scripts that NEVER error in production - even if there's an issue.
+$ErrorActionPreference = 'Stop'            
+
+# This makes the run Stop if attempting to use an unassigned variable. Lazy shits at City of Boise prefer scripts that NEVER error in production - even if there's an issue. How did I ever survive in this crap worthless world of hacks???
+
+Set-StrictMode -Version Latest
+try {
+  Write-Host $IdoNotExist
+}                        
+catch {
+  exit 9
+}
+exit 5 # Populates $LASTEXITCODE
