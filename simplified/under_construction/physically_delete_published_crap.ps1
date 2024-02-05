5c528d4a4fc188c2241c3d10d9147ceea16a320e
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-04 22:34:22 -0700

    Slowly coming together; not ready for prime time.

--- simplified/under_construction/physically_delete_published_crap.ps1
+++ simplified/under_construction/physically_delete_published_crap.ps1
@@ -15,8 +15,8 @@ param()
 
 # Track some stats. Useful for finding bugs. For instance, kept getting 12 new junction points, the same ones. turns out the test was bad.
 
-$howManyNewDirectories = 0
-$howManyUpdatedDirectories = 0
+$howManyPossibleCrapFiles = 0
+$howManyFilesWouldHaveBeenDeleted = 0
 $howManyDirectoriesFlaggedToScan = 0
 $howManyNewSymbolicLinks = 0
 $howManyNewJunctionLinks= 0
@@ -24,16 +24,63 @@ $hoWManyRowsUpdated = 0
 $hoWManyRowsInserted = 0
 $hoWManyRowsDeleted = 0
 
+$files_to_delete = @(
+    'Torrent_downloaded_from_Demonoid.me.txt',
+    '[TGx]Downloaded from torrentgalaxy.to .txt',
+    'Torrent downloaded from Demonoid - www demonoid pw .txt',
+    'How to play HEVC (THIS FILE).txt'
+)
+
+$files_to_keep = @(
+    'spud.txt', 'Read Me.txt', 'Info.txt', '_NO_SUBTITLES.txt'
+)              
+
+$extensions_to_delete = @('par2', 'IFO', 'BUP')
 # Fetch a string array of paths to search.
 
-$searchPathsHandle = Walk-Sql 'SELECT ' # All the directories across my volumes that I think have some sort of movie stuff in them.
-$searchPaths = $searchPathsHandle.Value
+$rando_download_files_to_ignore = @('cs', 'ps1', 'exe', 'ini', 'htm', 'h', 'idl')
+
+$possibleCrapFilesHandle = Walk-Sql "
+    SELECT
+        file_name_with_ext,
+        final_extension,
+        directory_path,
+        search_path_tag
+    FROM 
+        files_ext_v 
+    WHERE 
+        final_extension NOT IN('srt', 'mkv', 'avi', 'mp4', 'wmv', 'm4v', 'mov', 'mpeg', 'VOB', 'exe', 'sub', 'idx', 'AVI', 'zip') 
+    AND 
+        directly_deletable
+    AND
+        final_extension NOT IN('nfo')
+    AND NOT 
+        file_deleted
+    " 
+# All the directories across my volumes that I think have some sort of movie stuff in them.
+
+$possibleCrapFiles = $possibleCrapFilesHandle.Value
 
 # Search down each search path for directories that are different or missing from our data store.
 
-while ($searchPaths.Read()) {
-    $SearchPath = $searchPaths.GetString(0)
-    $SearchPathId = $searchPaths.GetInt32(1)
+while ($possibleCrapFiles.Read()) {
+    $howManyPossibleCrapFiles++
+    $file_name_with_ext = Get-SqlFieldValue $possibleCrapFilesHandle file_name_with_ext
+    $final_extension = Get-SqlFieldValue $possibleCrapFilesHandle final_extension
+    $directory_path = Get-SqlFieldValue $possibleCrapFilesHandle directory_path
+    $search_path_tag= Get-SqlFieldValue $possibleCrapFilesHandle search_path_tag
+
+    if ($search_path_tag -eq 'download' -and $final_extension -in $rando_download_files_to_ignore) {
+        # Ignore these
+    } 
+    if ($file_name_with_ext -cin $files_to_delete -and -not ($file_name_with_ext -cin $files_to_keep)) {
+        #Write-Host "DELETE $file_name_with_ext"
+    } elseif ($final_extension -cin $extensions_to_delete) {
+        #Write-Host "DELETE $file_name_with_ext"           
+    } else {
+        
+        @("Keep $file_name_with_ext", $directory_path)|Format-Table
+    }
     #Load first level of hierarchy
 }
 
@@ -41,14 +88,14 @@ while ($searchPaths.Read()) {
 # Display counts. If nothing is happening in certain areas, investigate.
 Write-Host # Get off the last nonewline
 Write-Host
-Write-Host "How many new directories were found:                      $howManyNewDirectories"           $(Format-Plural 'Directory' $howManyNewDirectories) 
-Write-Host "How many old directories were updated:                    $howManyUpdatedDirectories"       $(Format-Plural 'Directory' $howManyUpdatedDirectories) 
-Write-Host "How many rows were updated:                               $howManyRowsUpdated"              $(Format-Plural 'Row'       $howManyRowsUpdated) 
-Write-Host "How many rows were inserted:                              $hoWManyRowsInserted"             $(Format-Plural 'Row'       $hoWManyRowsInserted) 
-Write-Host "How many rows were deleted:                               $hoWManyRowsDeleted"              $(Format-Plural 'Row'       $hoWManyRowsDeleted) 
-Write-Host "How many new junction linked directories were found:      $howManyNewJunctionLinks"         $(Format-Plural 'Link'      $howManyNewJunctionLinks) 
-Write-Host "How many new symbolically linked directories were found:  $howManyNewSymbolicLinks"         $(Format-Plural 'Link'      $howManyNewSymbolicLinks) 
-Write-Host "How many directories were flagged for scanning:           $howManyDirectoriesFlaggedToScan" $(Format-Plural 'Directory' $howManyDirectoriesFlaggedToScan) 
+Write-Host "How many possible crap files were found:                  " $(Format-Plural 'File'      $howManyPossibleCrapFiles -includeCount) 
+Write-Host "How many files would have been deleted:                   " $(Format-Plural 'File'      $howManyFilesWouldHaveBeenDeleted -includeCount) 
+Write-Host "How many rows were updated:                               " $(Format-Plural 'Row'       $howManyRowsUpdated -includeCount) 
+Write-Host "How many rows were inserted:                              " $(Format-Plural 'Row'       $hoWManyRowsInserted -includeCount) 
+Write-Host "How many rows were deleted:                               " $(Format-Plural 'Row'       $hoWManyRowsDeleted -includeCount) 
+Write-Host "How many new junction linked directories were found:      " $(Format-Plural 'Link'      $howManyNewJunctionLinks -includeCount) 
+Write-Host "How many new symbolically linked directories were found:  " $(Format-Plural 'Link'      $howManyNewSymbolicLinks -includeCount) 
+Write-Host "How many directories were flagged for scanning:           " $(Format-Plural 'Directory' $howManyDirectoriesFlaggedToScan -includeCount) 
 #TODO: Update counts to session table
 
 # Da Fuutar!!!
