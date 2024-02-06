3736b74584c721b9a415249996ae624c0542c315
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-05 17:55:57 -0700

    Increasing quality: Exporting actual Exception/InnerException result code.

--- simplified/test idea/test_tasks/test_bad_syntax_w_trap_and_set_exitcode_env.ps1
+++ simplified/test idea/test_tasks/test_bad_syntax_w_trap_and_set_exitcode_env.ps1
@@ -11,10 +11,46 @@ $ErrorActionPreference = 'Stop'
 # This makes the run Stop if attempting to use an unassigned variable. Lazy shits at City of Boise prefer scripts that NEVER error in production - even if there's an issue. How did I ever survive in this crap worthless world of hacks???
 
 Set-StrictMode -Version Latest
-try {
-  Write-Host $IdoNotExist
-}                        
-catch {
-  exit 9
+$Exception = $null
+    
+switch (3) {
+  1 {try {Write-Host $IdoNotExist} catch {$Exception = $_.Exception }}# -2146233087
+  2 {try {1 / 0} catch {$Exception = $_.Exception } } # -2147352558
+  3 {[Int32]$i = [Int32]::MaxValue; try {$i + [Int32]::MaxValue|Out-Null } catch {$Exception = $_.Exception } } 
+  # 4: Connect to database
+  # 5 Read non-existent file
+  default {
+    
+  }
 }
-exit 5 # Populates $LASTEXITCODE
+                                                  
+$HResult = 0
+
+if ($Exception) {
+  if ($Exception.InnerException) {
+    $HResult = $Exception.InnerException.HResult # 
+  } else {
+    $HResult = $Exception.HResult
+  }                              
+  if ($Exception.ErrorRecord) { Write-Host "Error Record= $($Exception.ErrorRecord)"}
+  # ([Int32]"0x80131501") ==> -2146233087 CORRECT! What HResult was.
+  # EventData\Data\ResultCode=2148734209 "{0:X}" -f 2148734209 ==> 80131501 CORRECT. Do not use Format-Hex.
+}
+
+$HResult
+exit $HResult # Populates $LASTEXITCODE
+<#
+ErrorRecord                 : The variable '$IdoNotExist' cannot be retrieved because it has not been set.
+WasThrownFromThrowStatement : False
+TargetSite                  : Void CheckActionPreference(System.Management.Automation.Language.FunctionContext, System.Exception)
+Message                     : The variable '$IdoNotExist' cannot be retrieved because it has not been set.
+Data                        : {[System.Management.Automation.Interpreter.InterpretedFrameInfo, System.Management.Automation.Interpreter.InterpretedFrameInfo[]]}
+InnerException              :
+HelpLink                    :
+Source                      : System.Management.Automation
+HResult                     : -2146233087
+StackTrace                  :    at System.Management.Automation.ExceptionHandlingOps.CheckActionPreference(FunctionContext funcContext, Exception exception)
+                                 at System.Management.Automation.Interpreter.ActionCallInstruction`2.Run(InterpretedFrame frame)
+                                 at System.Management.Automation.Interpreter.EnterTryCatchFinallyInstruction.Run(InterpretedFrame frame)
+                                 at System.Management.Automation.Interpreter.EnterTryCatchFinallyInstruction.Run(InterpretedFrame frame)
+#>
