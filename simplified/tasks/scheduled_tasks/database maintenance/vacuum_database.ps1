b690ff3a10a900a59a2c31b87755e678ca61ea30
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-12 15:04:21 -0700

    Convert to specific relations (tables) so we don't vacuum the whole database, which rarely change. Took out the "FULL" attribute since it removes any details, I think, in the informational display, or does it only show work actually done??

--- simplified/tasks/scheduled_tasks/database maintenance/vacuum_database.ps1
+++ simplified/tasks/scheduled_tasks/database maintenance/vacuum_database.ps1
@@ -17,7 +17,14 @@ Log-Line "Starting Vacuum"
 ####### Sun Feb 11 17:17:19 MST 2024 "VACUUM" takes < 3 minutes
 ###### Sun Feb 11 18:17:39 MST 2024 VACUUM VERBOSE took a second after above.  Where does the output go?
 
-Invoke-Sql 'VACUUM (FULL, VERBOSE);' # Where does the output go?
+#Invoke-Sql 'VACUUM (FULL, VERBOSE);' # Where does the output go?
+#Invoke-Sql 'VACUUM (VERBOSE);' # Where does the output go?
+Invoke-Sql 'VACUUM (VERBOSE) files;' 
+Invoke-Sql 'VACUUM (VERBOSE) directories;' 
+Invoke-Sql 'VACUUM (VERBOSE) batch_run_sessions;' 
+Invoke-Sql 'VACUUM (VERBOSE) media_files;' 
+Invoke-Sql 'VACUUM (VERBOSE) video_files;' 
+Invoke-Sql 'VACUUM (VERBOSE) videos;' 
 Log-Line "Done Vacuuming"
                           
 . D:\qt_projects\filmcab\simplified\shared_code\_dot_include_standard_footer.ps1
