a605a25add2a8547f4d749dab03eeae92386dd5f
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-11 18:48:00 -0700

    Add a log line to see if vacuum ever finishes.

--- simplified/tasks/scheduled_tasks/database maintenance/vacuum_database.ps1
+++ simplified/tasks/scheduled_tasks/database maintenance/vacuum_database.ps1
@@ -11,8 +11,13 @@
  [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingCmdletAliases', '')]
  param()
  
- . D:\qt_projects\filmcab\simplified\shared_code\_dot_include_standard_header.ps1
- 
-Invoke-Sql 'VACUUM (FULL, VERBOSE);'                                           
+. D:\qt_projects\filmcab\simplified\shared_code\_dot_include_standard_header.ps1
+Log-Line "Starting Vacuum"                                                      
+####### Sun Feb 11 17:17:19 MST 2024 "VACUUM" takes < 3 minutes
+###### Sun Feb 11 18:17:39 MST 2024 VACUUM VERBOSE took a second after above.  Where does the output go?
+###### Sun Feb 11 17:17:35 MST 2024 "VACUUM (FULL, VERBOSE)" takes 31+ minutes
 
+Invoke-Sql 'VACUUM (FULL, VERBOSE);' # Where does the output go?
+Log-Line "Done Vacuuming"
+                          
 . D:\qt_projects\filmcab\simplified\shared_code\_dot_include_standard_footer.ps1
