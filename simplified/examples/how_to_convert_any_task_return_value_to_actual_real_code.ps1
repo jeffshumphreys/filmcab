7be94a102008bf78cf3effc477a90192a0f1e264
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-12 21:52:47 -0700

    Hammer and tongs rather than brain work.

---
+++ simplified/examples/how_to_convert_any_task_return_value_to_actual_real_code.ps1
@@ -0,0 +1,22 @@
+﻿        $code = [Int32]1
+        $returncode = 2147942401
+        #$returncode = 2147942402
+        $returncode = 4294967295
+        #$returncode = 4294967040
+        #$returncode = 4294967294
+        $returncode = 4294967040          # -256 Works
+
+        $realcodeexited = 0
+        if ($returncode -ge 65536 -and $returncode -le 2147942402) {
+            $transform = [bigint]::Pow(2,16)-1
+            $realcodeexited = $returncode -band $transform
+        }
+        else {
+            $transform = [bigint]::Pow(2,16)-1
+            #$realcodeexited = $returncode -bxor $transform
+            $realcodeexited = $returncode - 4294967296
+            # 4294967295 => -1
+            # 4294967040 => -65536
+        }            
+                                                              
+        Write-Host "Action return code $returncode is really $realcodeexited"
