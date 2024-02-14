1336c33efe0389d6b0da534ada594d75dcfa10cd
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-14 15:18:28 -0700

    KeyGestures?

---
+++ simplified/examples/weirdness.ps1
@@ -0,0 +1,8 @@
+﻿$fileProps = [unoidl.com.sun.star.beans.PropertyValue]::new() 
+
+<#
+Action                     DisplayName              Shortcut                        Submenus
+------                     -----------              --------                        --------
+Comment-IseSelectedLines   Comment Selected Lines   System.Windows.Input.KeyGesture {}      
+Uncomment-IseSelectedLines Uncomment Selected Lines System.Windows.Input.KeyGesture {}     
+#>
