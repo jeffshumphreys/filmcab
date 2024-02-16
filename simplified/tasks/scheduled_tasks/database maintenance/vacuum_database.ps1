b27bc1c7de27460d938e4ff142ea2749bbc32f6a
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-16 16:35:42 -0700

    Sweet As!  All perty and will run all new tables as well.

--- simplified/tasks/scheduled_tasks/database maintenance/vacuum_database.ps1
+++ simplified/tasks/scheduled_tasks/database maintenance/vacuum_database.ps1
@@ -1,8 +1,9 @@
 <#
  #    FilmCab Daily morning batch run process: Clean-up space left by updates and deletes.
  #    Called from Windows Task Scheduler, Task is in \FilmCab, Task name is same as file
- #    Status: Runs manually; prepping for lineup.
+ #    Status: Retesting in lineup
  #    ###### Tue Jan 23 18:23:11 MST 2024
+ #    ###### Fri Feb 16 16:32:24 MST 2024
  #    https://github.com/jeffshumphreys/filmcab/tree/master/simplified
  #
  #>
@@ -17,17 +18,12 @@ Log-Line "Starting Vacuum"
 ####### Sun Feb 11 17:17:19 MST 2024 "VACUUM" takes < 3 minutes
 ###### Sun Feb 11 18:17:39 MST 2024 VACUUM VERBOSE took a second after above.  Where does the output go?
 
-#Invoke-Sql 'VACUUM (FULL, VERBOSE);' # Where does the output go?
-#Invoke-Sql 'VACUUM (VERBOSE);' # Where does the output go?    
+$reader = WhileReadSql "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'simplified' ORDER BY tablename" 
 
-# While (While-Sql("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'simplified' ORDER BY tablename") {
-#     Invoke-Sql "VACUME $tablename"
-Invoke-Sql 'VACUUM (VERBOSE) files;' 
-Invoke-Sql 'VACUUM (VERBOSE) directories;' 
-Invoke-Sql 'VACUUM (VERBOSE) batch_run_sessions;' 
-Invoke-Sql 'VACUUM (VERBOSE) media_files;' 
-Invoke-Sql 'VACUUM (VERBOSE) video_files;' 
-Invoke-Sql 'VACUUM (VERBOSE) videos;' 
+While ($reader.Read()) {
+    Invoke-Sql "VACUUM (VERBOSE) $tablename"
+}
+
 Log-Line "Done Vacuuming"
                           
 . D:\qt_projects\filmcab\simplified\shared_code\_dot_include_standard_footer.ps1
