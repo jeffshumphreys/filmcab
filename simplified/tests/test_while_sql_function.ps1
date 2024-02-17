22c1da0d54ae6cd747840e4fece14af6a5dbc62e
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-16 17:34:33 -0700

    Reducio!

--- simplified/tests/test_while_sql_function.ps1
+++ simplified/tests/test_while_sql_function.ps1
@@ -4,21 +4,13 @@
  #    ###### Sat Feb 3 22:20:01 MST 2024
  #    https://github.com/jeffshumphreys/filmcab/tree/master/simplified
  #>
- 
- [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '')]
- [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingCmdletAliases', '')]
- param()
   
 . .\_dot_include_standard_header.ps1
  
-Start-Log
-
 $reader = WhileReadSql "SELECT 1 AS t" 
 
 While ($reader.Read()) {
     $t
 }
-
-Log-ScriptCompleted
  
 . .\_dot_include_standard_footer.ps1
