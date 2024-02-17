9e3ce9149418f77585a010c1868983cf7b3c8fcc
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-17 12:33:32 -0700

    Everything was out of order!

--- simplified/tasks/scheduled_tasks/file maintenance/zzz_end_batch_run_session.ps1
+++ simplified/tasks/scheduled_tasks/file maintenance/zzz_end_batch_run_session.ps1
@@ -3,41 +3,11 @@
  #    Called from Windows Task Scheduler, Task is in \FilmCab, Task name is same as file
  #    Status: Starting to work on
  #    ###### Tue Jan 23 18:23:11 MST 2024
+ #    Reviewed and refactored: ###### Sat Feb 17 12:02:04 MST 2024
  #    https://github.com/jeffshumphreys/filmcab/tree/master/simplified
  #>
- 
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '')]
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingCmdletAliases', '')]
-param()
 
 . .\_dot_include_standard_header.ps1
-                                   
-
-# Track some stats. Useful for finding bugs. For instance, kept getting 12 new junction points, the same ones. turns out the test was bad.
-
-# Check table: is it show as active?
-
-# How long did we run? Same day?
-
-# Did computer get rebooted since start of session?
-
-# Did all the tasks run? About the right time?
-
-# Unusual space amount eaten up?
-                                                                    
-# Check: $SanityCheckStatus|ConvertTo-Json|Out-File 'D:\qt_projects\filmcab\simplified\_data\__sanity_check_before_connection_before_session_starts.json'
-# Changed??  Shouldn't have.
-
-# Da Fuutar!!!
-. .\_dot_include_standard_footer.ps1
-
-
-. .\shared_code\__sanity_check_without_db_connection.ps1 'without_db_connection' 'after_session_ends'
-
-# Copy and date days sanity checks to history
-
-# Compare  D:\qt_projects\filmcab\simplified\_log\__sanity_checks\__sanity_check_before_connection_before_session_starts.json
-#      to  D:\qt_projects\filmcab\simplified\_log\__sanity_checks\__sanity_check_before_connection_after_session_ends.json
 
 $state_of_session = Out-SqlToDataset "SELECT batch_run_session_id, started FROM batch_run_sessions WHERE running"
 
@@ -60,3 +30,27 @@ elseif ($null -eq $state_of_session) {
     throw [Exception]"ERROR!: I'm running zzz_end_batch_run_session.ps1 AND NO SESSION IS ACTIVE!!!!"
 }
 
+# Track some stats. Useful for finding bugs. For instance, kept getting 12 new junction points, the same ones. turns out the test was bad.
+
+# Check table: is it show as active?
+
+# How long did we run? Same day?
+
+# Did computer get rebooted since start of session?
+
+# Did all the tasks run? About the right time?
+
+# Unusual space amount eaten up?
+                                                                    
+# Check: $SanityCheckStatus|ConvertTo-Json|Out-File 'D:\qt_projects\filmcab\simplified\_data\__sanity_check_before_connection_before_session_starts.json'
+# Changed??  Shouldn't have.
+
+# Da Fuutar!!!
+. .\_dot_include_standard_footer.ps1
+
+. .\__sanity_check_without_db_connection.ps1 'without_db_connection' 'after_session_ends'
+
+# Copy and date days sanity checks to history
+
+# Compare  D:\qt_projects\filmcab\simplified\_log\__sanity_checks\__sanity_check_before_connection_before_session_starts.json
+#      to  D:\qt_projects\filmcab\simplified\_log\__sanity_checks\__sanity_check_before_connection_after_session_ends.json
