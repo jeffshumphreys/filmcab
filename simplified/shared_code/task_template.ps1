2ee6566db9acf864886ec91d1a70e99017070008
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-16 17:34:20 -0700

    Much better template to base stuff on. Right Tabatha?

--- simplified/shared_code/task_template.ps1
+++ simplified/shared_code/task_template.ps1
@@ -11,41 +11,26 @@
 
 # Track some stats. Useful for finding bugs. For instance, kept getting 12 new junction points, the same ones. turns out the test was bad.
 
-$howManyNewDirectories = 0
-$howManyUpdatedDirectories = 0
-$howManyDirectoriesFlaggedToScan = 0
-$howManyNewSymbolicLinks = 0
-$howManyNewJunctionLinks= 0
-$hoWManyRowsUpdated = 0
-$hoWManyRowsInserted = 0
-$hoWManyRowsDeleted = 0
+$howManyNewDirectoriesWhereFound = 0
 
 # Fetch a string array of paths to search.
+                                                                
+$reader = WhileReadSql "SELECT search_directory, search_directory_id FROM search_directories ORDER BY search_directory_id" 
 
-$searchPathsHandle = Walk-Sql 'SELECT search_path, search_path_id FROM search_paths ORDER BY search_path_id' # All the directories across my volumes that I think have some sort of movie stuff in them.
-$searchPaths = $searchPathsHandle.Value
-
+While ($reader.Read()) {
+ 
 # Search down each search path for directories that are different or missing from our data store.
 
-while ($searchPaths.Read()) {
-    $SearchPath = $searchPaths.GetString(0)
-    $SearchPathId = $searchPaths.GetInt32(1)
-    #Load first level of hierarchy
+    $search_directory
+    $search_directory_id
+    $howManyNewDirectoriesWhereFound++
 }
 
 
 # Display counts. If nothing is happening in certain areas, investigate.
 Write-Host # Get off the last nonewline
 Write-Host
-Write-Host "How many new directories were found:                      $howManyNewDirectories"           $(Format-Plural 'Directory' $howManyNewDirectories) 
-Write-Host "How many old directories were updated:                    $howManyUpdatedDirectories"       $(Format-Plural 'Directory' $howManyUpdatedDirectories) 
-Write-Host "How many rows were updated:                               $howManyRowsUpdated"              $(Format-Plural 'Row'       $howManyRowsUpdated) 
-Write-Host "How many rows were inserted:                              $hoWManyRowsInserted"             $(Format-Plural 'Row'       $hoWManyRowsInserted) 
-Write-Host "How many rows were deleted:                               $hoWManyRowsDeleted"              $(Format-Plural 'Row'       $hoWManyRowsDeleted) 
-Write-Host "How many new junction linked directories were found:      $howManyNewJunctionLinks"         $(Format-Plural 'Link'      $howManyNewJunctionLinks) 
-Write-Host "How many new symbolically linked directories were found:  $howManyNewSymbolicLinks"         $(Format-Plural 'Link'      $howManyNewSymbolicLinks) 
-Write-Host "How many directories were flagged for scanning:           $howManyDirectoriesFlaggedToScan" $(Format-Plural 'Directory' $howManyDirectoriesFlaggedToScan) 
-#TODO: Update counts to session table
 
-# Da Fuutar!!!
+Write-Count howManyNewDirectoriesWhereFound Directory
+
 . .\_dot_include_standard_footer.ps1
