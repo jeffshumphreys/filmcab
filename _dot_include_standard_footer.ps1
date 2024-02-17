824e190e779fc8683a44a5abcf70164e357ea7ea
jeffshumphreys <jeffshumphreys@outlook.com>
2024-02-17 11:18:55 -0700

    Automated counts since every script has 'em

--- _dot_include_standard_footer.ps1
+++ _dot_include_standard_footer.ps1
@@ -9,6 +9,28 @@ param()
 
 Write-Host
 
+if ($Script:WriteCounts.Count -gt 0) {
+    $MaxLengthLabel = 0
+    $MaxNumber = 0
+                                 
+    Write-Host
+    ForEach($entry in $Script:WriteCounts) {
+        if ($entry.CountLabel.Length -gt $MaxLengthLabel) {$MaxLengthLabel = $entry.CountLabel.Length}
+        if ($entry.Count -gt $MaxNumber) {$MaxNumber = $entry.Count}
+    }
+     
+    $MaxNumberSpace = ($MaxNumber).ToString('N0')
+    $MaxNumberWidth = $MaxNumberSpace.Length
+
+    ForEach($entry in $Script:WriteCounts) {
+        if ($entry.CountLabel -ne '') {
+        $title = ($entry.CountLabel + ' '*($MaxLengthLabel)).Substring(0,$MaxLengthLabel)
+        $CountAsRightJustified = "$($entry.Count)".PadLeft($MaxNumberWidth)
+        Write-Host "$title    `:   $CountAsRightJustified"
+        }
+    }
+}   
+
 Format-Humanize $scriptTimer
 
 # Log-ScriptCompleted
