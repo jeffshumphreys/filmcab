8b8483ce002147845f3c15a01960f0791d12322d
jeffshumphreys <jeffshumphreys@outlook.com>
2024-03-06 11:21:44 -0700

    Added counts

--- simplified/under_construction/clean_up_table_data.ps1
+++ simplified/under_construction/clean_up_table_data.ps1
@@ -4,13 +4,16 @@
  #    Status: Conception
  #    ###### Sat Feb 3 22:20:01 MST 2024
  #    https://github.com/jeffshumphreys/filmcab/tree/master/simplified
+ #    NOTE: Seems to happen with brackets "[]" in the folder path.
  #>
 
  . .\_dot_include_standard_header.ps1
 
-Invoke-Sql "UPDATE directories SET folder = reverse((string_to_array(reverse(replace(directory_path::text, '\'::text, '\\'::text)), '\'))[1]) WHERE folder IS NULL"
-Invoke-Sql "UPDATE directories SET parent_folder = reverse((string_to_array(reverse(replace(directory_path::text, '\'::text, '\\'::text)), '\'))[2]) WHERE parent_folder IS NULL"
-Invoke-Sql "UPDATE directories SET grandparent_folder = reverse((string_to_array(reverse(replace(directory_path::text, '\'::text, '\\'::text)), '\'))[3]) WHERE grandparent_folder IS NULL"
+$HowManyFoldersPopulated = Invoke-Sql "UPDATE directories SET folder = reverse((string_to_array(reverse(replace(directory_path::text, '\'::text, '\\'::text)), '\'))[1]) WHERE folder IS NULL"
+$HowManyFoldersPopulated += Invoke-Sql "UPDATE directories SET parent_folder = reverse((string_to_array(reverse(replace(directory_path::text, '\'::text, '\\'::text)), '\'))[2]) WHERE parent_folder IS NULL"
+$HowManyFoldersPopulated += Invoke-Sql "UPDATE directories SET grandparent_folder = reverse((string_to_array(reverse(replace(directory_path::text, '\'::text, '\\'::text)), '\'))[3]) WHERE grandparent_folder IS NULL"
+                           
+Write-Count HowManyFoldersPopulated Folder
 
 . .\_dot_include_standard_footer.ps1
 
