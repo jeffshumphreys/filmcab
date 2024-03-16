3c8e0a349e07c2da93459ec9c954c0d3f69333b9
jeffshumphreys <jeffshumphreys@outlook.com>
2024-03-16 13:28:38 -0600

    Standardize and reduce SQL for conformity and readability

--- simplified/tasks/scheduled_tasks/file maintenance/populate_any_missing_file_hashes.ps1
+++ simplified/tasks/scheduled_tasks/file maintenance/populate_any_missing_file_hashes.ps1
@@ -14,42 +14,39 @@ $howManyUpdatedFiles = 0
 # Let's traverse all the undeleted directories flagged for scan. scan_for_file_directories sets the flag before this daily.
 
 if ($DatabaseConnectionIsOpen) {
-    $sql = "
-                    SELECT 
-                        d.directory_path || '\' || f.file_name_no_ext || CASE WHEN final_extension <> '' THEN '.' || final_extension ELSE '' END AS file_path,
-                        f.file_id                                                                                                                AS file_id
-                    FROM 
-                        files       f
-                    JOIN
-                        directories d  USING(directory_hash)
-                    WHERE 
-                        d.deleted IS DISTINCT FROM true    
-                    AND 
-                        f.is_symbolic_link IS DISTINCT FROM true
-                    AND
-                        f.is_hard_link IS DISTINCT FROM true
-                    AND
-                        f.file_hash IS NULL
+    $reader = WhileReadSql "
+        SELECT 
+            file_path,
+            file_id
+        FROM 
+            files_ext_v       
+        WHERE 
+            NOT directory_deleted 
+        AND 
+            NOT file_is_symbolic_link
+        AND
+            NOT file_is_hard_link
+        AND
+            file_hash IS NULL
     "
-    $reader = WhileReadSql $sql # Cannot return reader value directly from a function or it blanks, so return it boxed
 
     While ($reader.Read()) {
         if ((Test-Path $file_path)) {
             $on_fs_file_hash = (Get-FileHash -LiteralPath $file_path -Algorithm MD5).Hash
                     $update_sql = "
                         UPDATE
-                            files
+                            files_v
                         SET 
-                            file_hash         = '$on_fs_file_hash'::bytea
+                            file_hash   = '$on_fs_file_hash'::bytea
                         WHERE
-                            file_id = '$file_id'
+                            file_id     = $file_id
                     "
                     $howManyRowsUpdated = Invoke-Sql $update_sql
                     if ($howManyRowsUpdated -ne 1) {
                         throw [Exception]"Update failed to update anything or too many: $howManyRowsUpdated"
                     }                                                                  
 
-                    Write-AllPlaces '>' -NoNewline 
+                    _TICK_Existing_Object_Actually_Changed
                     $howManyUpdatedFiles++
         }
     } 
