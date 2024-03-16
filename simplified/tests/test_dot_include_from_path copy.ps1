258a4ae510ce9cee0aa85ae1b96b016e70931005
jeffshumphreys <jeffshumphreys@outlook.com>
2024-03-16 13:31:59 -0600

    testing on alternate data streams on media files.

---
+++ simplified/tests/test_dot_include_from_path copy.ps1
@@ -0,0 +1,270 @@
+General
+Count	348
+StreamCount	1
+StreamKind	General
+StreamKind/String	General
+StreamKindID	0
+UniqueID	128524050760136468212274187369588413450
+UniqueID/String 128524050760136468212274187369588413450 (0x60B0D4F067AE1B1EA3DD01BE2187540A)
+VideoCount	1
+AudioCount	1
+TextCount	1
+Video_Format_List	AVC
+Video_Format_WithHint_List	AVC
+Video_Codec_List	AVC
+Video_Language_List	en
+Audio_Format_List	AC-3
+Audio_Format_WithHint_List	AC-3
+Audio_Codec_List	AC-3
+Audio_Language_List	fr
+Audio_Channels_Total	2
+Text_Format_List	UTF-8
+Text_Format_WithHint_List	UTF-8
+Text_Codec_List UTF-8
+Text_Language_List	en
+Format	Matroska
+Format/String	Matroska
+Format/Url	https://matroska.org/downloads/windows.html
+Format/Extensions	mkv mk3d mka mks
+Format_Commercial	Matroska
+Format_Version	Version 2
+FileSize	1190640575
+FileSize/String 1.11 GiB
+FileSize/String1	1 GiB
+FileSize/String2	1.1 GiB
+FileSize/String3	1.11 GiB
+FileSize/String4	1.109 GiB
+Duration	4308000
+Duration/String 1h 11mn
+Duration/String1	1h 11mn 48s 0ms
+Duration/String2	1h 11mn
+Duration/String3	01:11:48.000
+Duration/String4	01:11:48:00
+Duration/String5	01:11:48.000 (01:11:48:00)
+OverallBitRate	2211032
+OverallBitRate/String	2211 Kbps
+FrameRate	24.000
+FrameRate/String	24.000 fps
+FrameCount	103392
+StreamSize	23693594
+StreamSize/String	22.6 MiB (2%)
+StreamSize/String1	23 MiB
+StreamSize/String2	23 MiB
+StreamSize/String3	22.6 MiB
+StreamSize/String4	22.60 MiB
+StreamSize/String5	22.6 MiB (2%)
+StreamSize_Proportion	0.01990
+IsStreamable	Yes
+Title	Fantastic.Planet.1973.720p.BRRip.x0r
+Movie	Fantastic.Planet.1973.720p.BRRip.x0r
+Encoded_Date	2011-03-19 14:45:45 UTC
+Encoded_Application	mkvmerge v2.9.7 ('Tenderness') built on Jul	5 2009 13:26:28
+Encoded_Application/String	mkvmerge v2.9.7 ('Tenderness') built on Jul	5 2009 13:26:28
+Encoded_Library libebml v0.7.8 + libmatroska v0.8.1
+Encoded_Library/String	libebml v0.7.8 + libmatroska v0.8.1
+Video
+Count	383
+StreamCount	1
+StreamKind	Video
+StreamKind/String	Video
+StreamKindID	0
+StreamOrder	0
+ID	1
+ID/String	1
+UniqueID	1900228792
+Format	AVC
+Format/String	AVC
+Format/Info	Advanced Video Codec
+Format/Url	http://developers.videolan.org/x264.html
+Format_Commercial	AVC
+Format_Profile	High@L3.1
+Format_Settings CABAC / 5 Ref Frames
+Format_Settings_CABAC	Yes
+Format_Settings_CABAC/String	Yes
+Format_Settings_RefFrames	5
+Format_Settings_RefFrames/String	5 frame
+InternetMediaType	video/H264
+CodecID V_MPEG4/ISO/AVC
+CodecID/Url	http://ffdshow-tryout.sourceforge.net/
+Duration	4308000
+Duration/String 1h 11mn
+Duration/String1	1h 11mn 48s 0ms
+Duration/String2	1h 11mn
+Duration/String3	01:11:48.000
+Duration/String4	01:11:48:00
+Duration/String5	01:11:48.000 (01:11:48:00)
+BitRate 2050000
+BitRate/String	2050 Kbps
+Width	1200
+Width/String	1200 pixel
+Height	720
+Height/String	720 pixel
+Sampled_Width	1200
+Sampled_Height	720
+PixelAspectRatio	1.000
+DisplayAspectRatio	1.667
+DisplayAspectRatio/String	5:3
+FrameRate_Mode	CFR
+FrameRate_Mode/String	CFR
+FrameRate_Mode_Original VFR
+FrameRate	24.000
+FrameRate/String	24.000 fps
+FrameCount	103392
+ColorSpace	YUV
+ChromaSubsampling	4:2:0
+ChromaSubsampling/String	4:2:0
+BitDepth	8
+BitDepth/String 8 bit
+ScanType	Progressive
+ScanType/String Progressive
+Bits-(Pixel*Frame)	0.099
+Delay	0
+Delay/String3	00:00:00.000
+Delay/String4	00:00:00:00
+Delay/String5	00:00:00.000 (00:00:00:00)
+Delay_Source	Container
+Delay_Source/String	Container
+StreamSize	1080786981
+StreamSize/String	1.01 GiB (91%)
+StreamSize/String1	1 GiB
+StreamSize/String2	1.0 GiB
+StreamSize/String3	1.01 GiB
+StreamSize/String4	1.007 GiB
+StreamSize/String5	1.01 GiB (91%)
+StreamSize_Proportion	0.90774
+Encoded_Library x264 - core 112
+Encoded_Library/String	x264 core 112
+Encoded_Library_Name	x264
+Encoded_Library_Version core 112
+Encoded_Library_Settings	cabac=1 / ref=5 / deblock=1:-2:-1 / analyse=0x3:0x113 / me=umh / subme=7 / psy=1 / psy_rd=1.00:0.00 / mixed_ref=1 / me_range=16 / chroma_me=1 / trellis=1 / 8x8dct=1 / cqm=0 / deadzone=21,11 / fast_pskip=1 / chroma_qp_offset=-2 / threads=3 / sliced_threads=0 / nr=0 / decimate=1 / interlaced=0 / constrained_intra=0 / bframes=5 / b_pyramid=2 / b_adapt=2 / b_bias=0 / direct=1 / weightb=1 / open_gop=0 / weightp=2 / keyint=240 / keyint_min=24 / scenecut=40 / intra_refresh=0 / rc_lookahead=50 / rc=2pass / mbtree=1 / bitrate=2050 / ratetol=1.0 / qcomp=0.60 / qpmin=3 / qpmax=51 / qpstep=4 / cplxblur=20.0 / qblur=0.5 / ip_ratio=1.40 / aq=1:1.00
+Language	en
+Language/String en
+Language/String1	en
+Language/String2	en
+Language/String3	eng
+Language/String4	en
+Default Yes
+Default/String	Yes
+Forced	No
+Forced/String	No
+colour_description_present	Yes
+colour_description_present_Sourc	Stream
+colour_range	Limited
+colour_range_Source	Stream
+colour_primaries	BT.709
+colour_primaries_Source Stream
+transfer_characteristics	BT.709
+transfer_characteristics_Source Stream
+matrix_coefficients	BT.709
+matrix_coefficients_Source	Stream
+FrameCount_Source	General_Duration
+Duration_Source General_Duration
+Audio
+Count	300
+StreamCount	1
+StreamKind	Audio
+StreamKind/String	Audio
+StreamKindID	0
+StreamOrder	1
+ID	2
+ID/String	2
+UniqueID	1079654773
+Format	AC-3
+Format/String	AC-3
+Format/Info	Audio Coding 3
+Format/Url	https://en.wikipedia.org/wiki/AC3
+Format_Commercial	Dolby Digital
+Format_Commercial_IfAny Dolby Digital
+Format_Settings_Endianness	Big
+CodecID A_AC3
+Duration	4308000
+Duration/String 1h 11mn
+Duration/String1	1h 11mn 48s 0ms
+Duration/String2	1h 11mn
+Duration/String3	01:11:48.000
+Duration/String5	01:11:48.000
+BitRate_Mode	CBR
+BitRate_Mode/String	CBR
+BitRate 160000
+BitRate/String	160 Kbps
+Channel(s)	2
+Channel(s)/String	2 channel
+ChannelPositions	Front: L R
+ChannelPositions/String2	2/0/0
+ChannelLayout	L R
+SamplesPerFrame 1536
+SamplingRate	48000
+SamplingRate/String	48.0 KHz
+SamplingCount	206784000
+FrameRate	31.250
+FrameRate/String	31.250 fps (1536 SPF)
+Compression_Mode	Lossy
+Compression_Mode/String Lossy
+Delay	0
+Delay/String3	00:00:00.000
+Delay/String5	00:00:00.000
+Delay_Source	Container
+Delay_Source/String	Container
+Video_Delay	0
+Video_Delay/String3	00:00:00.000
+Video_Delay/String5	00:00:00.000
+StreamSize	86160000
+StreamSize/String	82.2 MiB (7%)
+StreamSize/String1	82 MiB
+StreamSize/String2	82 MiB
+StreamSize/String3	82.2 MiB
+StreamSize/String4	82.17 MiB
+StreamSize/String5	82.2 MiB (7%)
+StreamSize_Proportion	0.07236
+Language	fr
+Language/String fr
+Language/String1	fr
+Language/String2	fr
+Language/String3	fra
+Language/String4	fr
+ServiceKind	CM
+ServiceKind/String	Complete Main
+Default Yes
+Default/String	Yes
+Forced	No
+Forced/String	No
+bsid	8
+dialnorm	-31
+dialnorm	-31 dB
+dsurmod 0
+acmod	2
+lfeon	0
+dialnorm_Average	-31
+dialnorm_Average	-31 dB
+dialnorm_Minimum	-31
+dialnorm_Minimum	-31 dB
+dialnorm_Maximum	-31
+dialnorm_Maximum	-31 dB
+dialnorm_Count	872
+SamplingCount_Source	General_Duration
+Duration_Source General_Duration
+Text
+Count	304
+StreamCount	1
+StreamKind	Text
+StreamKind/String	Text
+StreamKindID	0
+StreamOrder	2
+ID	3
+ID/String	3
+UniqueID	756151009
+Format	UTF-8
+Format/String	UTF-8
+Format_Commercial	UTF-8
+CodecID S_TEXT/UTF8
+CodecID/Info	UTF-8 Plain Text
+Language	en
+Language/String en
+Language/String1	en
+Language/String2	en
+Language/String3	eng
+Language/String4	en
+Default Yes
+Default/String	Yes
+Forced	No
+Forced/String	No
