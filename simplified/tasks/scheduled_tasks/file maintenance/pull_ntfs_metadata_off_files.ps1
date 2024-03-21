01e9be1aceb1dc6ac8b5f8710092b02c9cf4352b
jeffshumphreys <jeffshumphreys@outlook.com>
2024-03-21 14:26:17 -0600

    #77 Fixed so it compares a proper ASCII id to the on_fs ASCII id. It was updating every row,.

--- simplified/tasks/scheduled_tasks/file maintenance/pull_ntfs_metadata_off_files.ps1
+++ simplified/tasks/scheduled_tasks/file maintenance/pull_ntfs_metadata_off_files.ps1
@@ -4,12 +4,14 @@
  #    Status: Concept
  #    ###### Tue Jan 23 18:23:11 MST 2024
  #    https://github.com/jeffshumphreys/filmcab/tree/master/simplified
-  #>
+ #
+ # ###### Thu Mar 21 13:44:58 MDT 2024 Took 10 minutes to go through 40,489 real files and no updates since ntfs_ids matched.  Not bad but I hate redundancy.
+ #>
 
 try {
 . .\_dot_include_standard_header.ps1
 
-$howManyUpdatedFiles     = 0
+$howManyUpdatedFiles = 0
 
 # Let's traverse all the undeleted directories flagged for scan. scan_for_file_directories sets the flag before this daily.
 
@@ -18,30 +20,29 @@ $walkThruAllFilesReader = WhileReadSql <#sql#>"
         file_id
     ,   file_path
     ,   file_ntfs_id         AS in_db_file_ntfs_id
+    ,   how_many_real_files /* Use this for progress bar is possible. */
     FROM 
         files_ext_v
     WHERE
-        NOT directory_deleted
-    AND
-        NOT directory_is_symbolic_link
-    AND
-        NOT directory_is_junction_link
-    AND                      
-        NOT file_deleted
-    AND
-        NOT file_is_symbolic_link
-    AND
-        NOT file_is_hard_link
+        is_real_file
 "
-
+                                                                        
 While ($walkThruAllFilesReader.Read()) {
     if ((Test-Path -LiteralPath $file_path)) {
-        _TICK_Existing_Object_Edited
+        _TICK_Found_Existing_Object
         $on_fs_file_ntfs_id = (fsutil file queryfileid $file_path).Substring(13) # Trim off annoying lead text
+        $in_db_file_ntfs_id = Convert-ByteArrayToHexString ($in_db_file_ntfs_id)
 
         if ($null -eq $on_fs_file_ntfs_id -or $on_fs_file_ntfs_id -ne $in_db_file_ntfs_id) {
             _TICK_Existing_Object_Actually_Changed
-            Invoke-Sql "UPDATE files_v SET file_ntfs_id = '$on_fs_file_ntfs_id'::bytea WHERE file_id = $file_id"|Out-Null
+            Invoke-Sql "
+                UPDATE 
+                    files_v 
+                SET 
+                    file_ntfs_id = '$on_fs_file_ntfs_id'::bytea 
+                WHERE 
+                    file_id = $file_id
+            "|Out-Null
             $howManyUpdatedFiles++
         }
             
