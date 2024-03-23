8140cb07ff5e673ca7b3c7f852f43251da1724bc
jeffshumphreys <jeffshumphreys@outlook.com>
2024-03-23 13:38:51 -0600

    align with refactor and view wrapper: Refactor of names with "stopped" to "ended" #80

--- simplified/tasks/scheduled_tasks/file maintenance/zzz_end_batch_run_session.ps1
+++ simplified/tasks/scheduled_tasks/file maintenance/zzz_end_batch_run_session.ps1
@@ -10,13 +10,13 @@
 try {
 . .\_dot_include_standard_header.ps1
 
-$state_of_session = Out-SqlToDataset "SELECT batch_run_session_id, started FROM batch_run_sessions WHERE running"
+$state_of_session = Out-SqlToDataset "SELECT batch_run_session_id, started FROM batch_run_sessions_v WHERE running"
 
 # Check if old session still marked as active
                                                                          
 if ($null -ne $state_of_session -and $state_of_session -isnot [String] -and $state_of_session.Count -eq 1) {
     if ( $state_of_session -is [System.Data.DataRow]) { # Out-SqlToDataset returns an object if only one row, hence
-        Invoke-Sql "UPDATE batch_run_sessions SET running = NULL, session_killing_script = '$ScriptName', stopped = CURRENT_TIMESTAMP, caller_stopping = '$Script:Caller' WHERE running" | Out-Null
+        Invoke-Sql "UPDATE batch_run_sessions_v SET running = NULL, session_ending_script = '$ScriptName', ended = CURRENT_TIMESTAMP, caller_ending = '$Script:Caller' WHERE running" | Out-Null
     } else {
         Show-Error -message "ERROR!: I'm running zzz_end_batch_run_session.ps1 AND NO SESSION IS ACTIVE!!!! (1)" -exitcode 2
     }
@@ -28,14 +28,14 @@ elseif ($null -ne $state_of_session -and $state_of_session -isnot [String] -and 
 
         # Flush out the active marked record so we can start a new session.
         
-        Invoke-Sql "UPDATE batch_run_sessions SET running = NULL, session_killing_script = '$ScriptName', stopped = CURRENT_TIMESTAMP, caller_stopping = '$Script:Caller' WHERE running" | Out-Null
+        Invoke-Sql "UPDATE batch_run_sessions_v SET running = NULL, session_ending_script = '$ScriptName', ended = CURRENT_TIMESTAMP, caller_ending = '$Script:Caller' WHERE running" | Out-Null
     } else {
         Show-Error -message "ERROR!: I'm running zzz_end_batch_run_session.ps1 AND UNRECOGNIZED TYPE SITU!" -exitcode 3
     }
 }                                                                          
 elseif ($null -ne $state_of_session -and $state_of_session -isnot [String] -and $state_of_session.Table.Count -gt 2) {                         
     # Broken table constraint, only possibility, so note it and crash.
-    Show-Error -message "ERROR: More than one session marked active: Query was 'SELECT batch_run_session_id, started FROM batch_run_sessions WHERE running', STOPPING!" -exitcode 4
+    Show-Error -message "ERROR: More than one session marked active: Query was 'SELECT batch_run_session_id, started FROM batch_run_sessions_v WHERE running', STOPPING!" -exitcode 4
 }                                                                                                                                                                         
 elseif ($null -eq $state_of_session -or $state_of_session -is [String]) {
     # No session active??
