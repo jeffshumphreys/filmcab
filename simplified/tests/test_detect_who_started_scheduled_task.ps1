9cd5eafb80ecfd5989012d7cae1c0f15f0f9e311
jeffshumphreys <jeffshumphreys@outlook.com>
2024-03-31 14:23:09 -0600

    added some notes for a confusing code bit

--- simplified/tests/test_detect_who_started_scheduled_task.ps1
+++ simplified/tests/test_detect_who_started_scheduled_task.ps1
@@ -5,12 +5,15 @@
  #    https://github.com/jeffshumphreys/filmcab/tree/master/simplified
  #>
 
-# Fake out Start-Log
-$Script:TestScheduleDrivenTaskDetection = $true
+# Fake out Start-Log for testing purposes.  You can't capture Windows Task Scheduler live events from a test, so fake!
+
+$Script:TestScheduleDrivenTaskDetection     = $true
 $Script:PretendMyFileNameWithoutExtensionIs = 'nWizard_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}'          # Logon
 $Script:PretendMyFileNameWithoutExtensionIs = 'MicrosoftEdgeUpdateTaskMachineCore'                      # event (Really is an Idle trigger!!!!!)
-$Script:PretendMyFileNameWithoutExtensionIs = 'back_up_unbackedup_published_media'                      # event
 $Script:PretendMyFileNameWithoutExtensionIs = '_start_new_batch_run_session'                            # scheduler, user
+$Script:PretendMyFileNameWithoutExtensionIs = 'back_up_unbackedup_published_media'                      # event
+
+#WARNING: IF No active sessions, header code stuffs this on #1!!!! So testing can happen!
 
  . .\_dot_include_standard_header.ps1
  
