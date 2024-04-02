e7780f34368a571a8038dbd2cfe0e5fb715b578b
jeffshumphreys <jeffshumphreys@outlook.com>
2024-04-02 15:49:56 -0600

    Improved; fixed bug in run_duration calc; migrated out of GENERATED

--- simplified/sql/Rebuild batch_run_sessions dependency tree.sql
+++ simplified/sql/Rebuild batch_run_sessions dependency tree.sql
@@ -2,36 +2,32 @@
 DROP VIEW IF EXISTS batch_run_sessions_v CASCADE;
 CREATE OR REPLACE VIEW simplified.batch_run_sessions_v
 AS SELECT 
-    brs.batch_run_session_id,
-    brs.started,
-    brs.stopped                       AS ended,
-    brs.marking_stopped_after_overrun AS marking_ended_after_overrun,
-    brs.running,
-    brs.run_duration_in_seconds,
-    brs.last_script_ran,
-    brs.session_starting_script,
-    brs.session_killing_script        AS session_ending_script,
-    brs.caller_starting,
-    brs.caller_stopping               AS caller_ending,
-    brs.trigger_type,
-    brs.triggered_by_login                                                                                                                                                                                       AS triggered_by_login
-   FROM batch_run_sessions brs;
+    batch_run_session_id                           AS batch_run_session_id,
+    started                                        AS started,
+    initcap(to_char(started, 'DAY'))               AS started_on_dow,          
+    stopped                                        AS ended,
+    marking_stopped_after_overrun                  AS marking_ended_after_overrun,
+    running                                        AS running,
+    TRUNC(EXTRACT(EPOCH FROM (stopped - started))) AS run_duration_in_seconds,
+    TRUNC(EXTRACT(EPOCH FROM (stopped - started))/60) AS run_duration_in_minutes,
+    TRUNC(EXTRACT(EPOCH FROM (stopped - started))/60/60,2) AS run_duration_in_hours,
+    last_script_ran                                AS last_script_ran,
+    session_starting_script                        AS session_starting_script,
+    session_killing_script                         AS session_ending_script,
+    caller_starting                                AS caller_starting,
+    caller_stopping                                AS caller_ending,
+    trigger_type                                   AS trigger_type,
+    triggered_by_login                             AS triggered_by_login,
+    thread_id,
+    process_id
+    activity_uuid,
+    trigger_id
+   FROM batch_run_sessions;
    
 -- simplified.batch_run_sessions_v_last_10_days_v source
 
 CREATE OR REPLACE VIEW simplified.batch_run_sessions_v_last_10_days_v
-AS SELECT 
-    batch_run_session_id,
-    started,
-    ended,
-    marking_ended_after_overrun,
-    running,
-    run_duration_in_seconds,
-    last_script_ran,
-    session_starting_script,
-    session_ending_script,
-    caller_starting,
-    caller_ending
+AS SELECT *
    FROM batch_run_sessions_v
   WHERE started > (CURRENT_DATE - '10 days'::interval)
   ORDER BY started;
@@ -39,18 +35,7 @@ AS SELECT
 -- simplified.batch_run_sessions_scheduled_and_completed_v source
 
 CREATE OR REPLACE VIEW simplified.batch_run_sessions_scheduled_and_completed_v
-AS SELECT 
-    batch_run_session_id,
-    started,
-    ended,
-    marking_ended_after_overrun,
-    running,
-    run_duration_in_seconds,
-    last_script_ran,
-    session_starting_script,
-    session_ending_script,
-    caller_starting,
-    caller_ending
+AS SELECT *
    FROM batch_run_sessions_v
   WHERE 
     started IS NOT NULL 
@@ -67,3 +52,5 @@ AS SELECT
   AND 
     caller_ending = 'Windows Task Scheduler'
   ORDER BY started;
+  
+SELECT * FROM batch_run_sessions_scheduled_and_completed_v brssacv  
