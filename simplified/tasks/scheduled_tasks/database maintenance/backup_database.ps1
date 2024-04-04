d3bdc9a752e3cccb611b4c5c845c6e86fb2ec637
jeffshumphreys <jeffshumphreys@outlook.com>
2024-04-04 11:52:18 -0600

    more display output; more comments; removed hardcoded schemas;fixed bug with files dump;

--- simplified/tasks/scheduled_tasks/database maintenance/backup_database.ps1
+++ simplified/tasks/scheduled_tasks/database maintenance/backup_database.ps1
@@ -10,17 +10,19 @@
  try {
 . .\_dot_include_standard_header.ps1
 
-$RidiculousLongTimestamp = (Get-Date).DateTime -replace ':', ' '
+$RidiculousLongTimestamp = (Get-Date).DateTime -replace ':', ' ' # ":" is an illegal file name character
 
 # format c = Output a custom-format archive suitable for input into pg_restore. Together with the directory output format, this is the most flexible output format in that it allows manual selection and reordering of archived items during restore. This format is also compressed by default
 
-$file_name_base = "$($Config.backup_path)/dump-$($Config.database)-database-data-$($Config.schema)-schema.$RidiculousLongTimestamp.sql"
+$file_name_base = "$($Config.backup_path)/dump-$($Config.database)-database-data-$($Config.database_schema)-schema.$RidiculousLongTimestamp.sql"
       
 Write-AllPlaces "base to all backups: $file_name_base"
                                                     
 $backup_file_path = "$file_name_base-compressed.sql"
 #TODO: Convert following to function
-$output = & pg_dump.exe --verbose --format=c --file "$backup_file_path" --dbname=filmcab --schema=simplified 2>&1 
+$output = & pg_dump.exe --verbose --format=custom --file "$backup_file_path" --dbname=$($Config.database) --schema=$($Config.database_schema) 2>&1 
+Write-AllPlaces "(1) pg_dump to $backup_file_path completed"
+
 $stdout = $output | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
 $stderr = $output | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
             
@@ -38,14 +40,20 @@ if ($null -ne $stderr) {
 Get-Item $backup_file_path|Select ResolvedTarget, Name, Length, CreationTime, CreationTimeUTC, Attributes
 # format p = simple text
 
-pg_dump.exe --verbose --format=p --file "$file_name_base-in_text.sql" --dbname=filmcab --schema=simplified --blobs|Out-Host
+$targetPath = "$file_name_base-in_text.sql"
+pg_dump.exe --verbose --format=plain --file "$targetPath"  --dbname=$($Config.database) --schema=$($Config.database_schema) --blobs|Out-Host
+Write-AllPlaces "(2) pg_dump to $targetPath completed"
 
-pg_dump.exe --verbose --format=p --file "$file_name_base-table-files-in_text.sql" --dbname=filmcab --schema=simplified --table=files --blobs|Out-Host
-
-$file_name_base = "$($Config.backup_path)/dump-$($Config.database)-database-$($Config.schema)-schema-only.$RidiculousLongTimestamp.sql"
-$file_name_to_codebase = "$($Config.backup_path)/dump-$($Config.database)-database-$($Config.schema)-schema-only.sql"
-                                                                       
-pg_dump.exe --verbose --format=p --file "$file_name_base-in_text.sql" --schema-only --dbname=filmcab --schema=simplified --blobs|Out-Host
+$targetPath = "$file_name_base-table-files-in_text.sql"
+pg_dump.exe --verbose --format=plain --file "$targetPath" --dbname=$($Config.database) --schema=$($Config.database_schema) --table=$($Config.database_schema).files --blobs|Out-Host
+Write-AllPlaces "(3) pg_dump to $targetPath completed"
+                                                                                     
+$file_name_base        = "$($Config.backup_path)/dump-$($Config.database)-database-$($Config.database_schema)-schema-only.$RidiculousLongTimestamp.sql"
+$file_name_to_codebase = "$($Config.backup_path)/dump-$($Config.database)-database-$($Config.database_schema)-schema-only.sql"
+                                            
+$targetPath = "$file_name_base-in_text.sql"
+pg_dump.exe --verbose --format=plain --file "$targetPath" --schema-only --dbname=$($Config.database) --schema=$($Config.database_schema) --blobs|Out-Host
+Write-AllPlaces "(4) pg_dump to $targetPath completed"
 
 $schema = Get-Content "$file_name_base-in_text.sql"
 $date_free_schema = @()
@@ -59,17 +67,20 @@ $date_free_schema|Set-Content -Path $file_name_to_codebase
 
 $path_base_for_all = "$($Config.local_path)\$($Config.subfolder)"
 
-$file_name_in_codebase               = "$path_base_for_all/sql/dump-$($Config.database)-database-$($Config.schema)-schema-only.sql"
-$file_name_in_codebase_previous_copy = "$path_base_for_all/sql/dump-$($Config.database)-database-$($Config.schema)-schema-only.prev.sql"
-    
+$file_name_in_codebase               = "$path_base_for_all/sql/dump-$($Config.database)-database-$($Config.database_schema)-schema-only.sql"
+$file_name_in_codebase_previous_copy = "$path_base_for_all/sql/dump-$($Config.database)-database-$($Config.database_schema)-schema-only.prev.sql"
+Write-AllPlaces "`$file_name_in_codebase = $file_name_in_codebase"
+Write-AllPlaces "`$file_name_in_codebase_previous_copy = $file_name_in_codebase_previous_copy"
+
+# Error: Following doesn't detect MATERIAL differences due to crap in the file
+
 $previous_sql_hash = (Get-FileHash -LiteralPath $file_name_to_codebase -Algorithm MD5).Hash
-$new_sql_hash = (Get-FileHash -LiteralPath $file_name_in_codebase -Algorithm MD5).Hash
+$new_sql_hash      = (Get-FileHash -LiteralPath $file_name_in_codebase -Algorithm MD5).Hash
 
 if ($previous_sql_hash -ne $new_sql_hash) {
     Write-AllPlaces "Difference between new and last DDL detected."
     Copy-Item $file_name_in_codebase -Destination $file_name_in_codebase_previous_copy -Force -Verbose
     Copy-Item $file_name_to_codebase -Destination $file_name_in_codebase -Force -Verbose # Will trigger github changes    
-    Copy-Item $file_name_to_codebase -Destination $file_name_in_codebase -Force -Verbose
 } else {
     Write-AllPlaces "No difference between new and last DDL detected."
 }
