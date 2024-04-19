4e23090394aaae3e5327573640799b5b7aefca2a
jeffshumphreys <jeffshumphreys@outlook.com>
2024-04-19 13:37:23 -0600

    more failed tests

---
+++ simplified/tests/test_fluent_xaml.ps1
@@ -0,0 +1,36 @@
+﻿$job = Start-Job -ScriptBlock {
+$source = @"
+public class ConstructXAML
+{
+    public ConstructXAML() {
+        constructedXAML = "";
+    }
+
+    public string constructedXAML = "";
+
+    public ConstructXAML VerticalPanel()
+    {
+        constructedXAML = "<StackPanel Orientation=\"Vertical\">" + constructedXAML + "</StackPanel>";
+        return this; 
+    }
+
+    public ConstructXAML Border(int height = 40) {
+        constructedXAML = `$"<Border BorderBrush={height}\" Margin=\"1,1,1,1\" VerticalAlignment=\"Top\" Width=\"132\">" + constructedXAML + "</Border>";
+        return this; 
+        
+    }
+
+    public override string ToString() {
+        return constructedXAML;
+    }
+}
+"@
+
+
+Add-Type -TypeDefinition $source -Language CSharp
+$xaml = [ConstructXAML]::New()
+$xaml.VerticalPanel().Border(1)
+}
+
+Receive-Job $job -Wait
+
---
+++ simplified/tests/test_fuent.ps1
@@ -0,0 +1,25 @@
+﻿$source = @"
+public class ConstructXAML
+{
+    public ConstructXAML() {
+        constructedXAML = "";
+    }
+
+    public string constructedXAML = "";
+    public ConstructXAML VerticalPanel()
+    {
+        constructedXAML = "<StackPanel Orientation=\"Vertical\">" + constructedXAML + "</StackPanel>";
+        return this; 
+    }
+    public override string ToString() {
+        return constructedXAML;
+    }
+}
+"@
+
+Add-Type -TypeDefinition $source -Language CSharp
+$xaml = [ConstructXAML]::New()
+$xaml.VerticalPanel()
+$xaml.VerticalPanel()
+$xaml.constructedXAML
+
