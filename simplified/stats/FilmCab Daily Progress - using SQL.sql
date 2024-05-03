c84dc47a615aeb49ad7632b4179ca75e75762794
jeffshumphreys <jeffshumphreys@outlook.com>
2024-05-02 19:12:49 -0600

    fiddle, but this got moved to a ps file

--- simplified/stats/FilmCab Daily Progress - using SQL.sql
+++ simplified/stats/FilmCab Daily Progress - using SQL.sql
@@ -3,7 +3,8 @@ SELECT
   (SELECT count(*)          AS "How many files" FROM files), 
   (SELECT count(*)          AS "How many videos watched" FROM user_spreadsheet_interface usi WHERE seen = 'y'),
   (SELECT count(*)          AS "have" FROM user_spreadsheet_interface usi WHERE have = 'y'),
-  (SELECT sum(row_count)    AS "How many rows" FROM (SELECT table_name, (SELECT n_live_tup FROM pg_stat_user_tables WHERE relname = table_name AND schemaname = 'simplified') AS row_count FROM information_schema. tables WHERE table_schema = 'simplified') AS "How many rows"),
+  (SELECT sum(row_count)    AS "How many rows" FROM (SELECT table_name, 
+    (SELECT n_live_tup FROM pg_stat_user_tables WHERE relname = table_name AND schemaname = 'simplified') AS row_count FROM information_schema. tables WHERE table_schema = 'simplified') AS "How many rows"),
   (SELECT round(SUM(bytes_moved) / 1000/1000/1000, 2) 
                             AS "Move Size (GB)" FROM moves),
   (SELECT sum(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::bigint / 1000.0 / 1000.0 
@@ -20,4 +21,16 @@ select pg_size_pretty(pg_database_size('filmcab'));
 SELECT table_name, (SELECT n_live_tup FROM pg_stat_user_tables WHERE relname = table_name AND schemaname = 'simplified') AS row_count FROM information_schema. tables WHERE table_schema = 'simplified';
 SELECT * FROM pg_stat_user_tables ;
 show data_directory;
-ALTER TABLE scheduled_tasks ADD COLUMN is_enabled BOOL DEFAULT(True);
+    SELECT
+        volume_id
+    ,   drive_letter
+    ,   max(tag) as tag
+    FROM
+        search_directories_ext_v
+    GROUP BY
+        volume_id
+    ,   drive_letter
+    ORDER BY
+        1;
+    
+        SELECT run_duration_in_minutes  FROM batch_run_sessions_v brsv WHERE session_ending_script = 'zzz_end_batch_run_session.ps1' ORDER BY started DESC LIMIT 1
