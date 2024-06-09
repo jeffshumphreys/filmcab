bbf1472524d71f921d59bcd53e7e14e4f1b1bc58
jeffshumphreys <jeffshumphreys@outlook.com>
2024-06-09 16:31:45 -0600

    Thought my smb was misconfigured.

---
+++ simplified/tests/test_smb1.ps1
@@ -0,0 +1,5 @@
+﻿Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+Get-SmbServerConfiguration
+Set-SmbServerConfiguration -EnableSMB2Protocol $false
+Set-SmbServerConfiguration -AnnounceServer $true
