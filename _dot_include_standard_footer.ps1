079d5e09c65d2b846f558b9e8356bfc01c3d6c11
jeffshumphreys <jeffshumphreys@outlook.com>
2024-07-12 16:44:02 -0600

    remove spaces, blank lines

--- _dot_include_standard_footer.ps1
+++ _dot_include_standard_footer.ps1
@@ -1,6 +1,6 @@
 <#
     Tied/Paired with _dot_include_standard_header.ps1. Won't work if header not included.
-#>                                                                                       
+#>
 
 try {
 Write-AllPlaces
@@ -8,13 +8,13 @@ Write-AllPlaces
 if ((Test-Path variable:script:WriteCounts) -and $Script:WriteCounts.Count -gt 0) {
     $MaxLengthLabel = 0
     $MaxNumber = 0
-                                 
+
     Write-AllPlaces
     ForEach($entry in $Script:WriteCounts) {
         if ($entry.CountLabel.Length -gt $MaxLengthLabel) {$MaxLengthLabel = $entry.CountLabel.Length}
         if ($entry.Count -gt $MaxNumber) {$MaxNumber = $entry.Count}
     }
-     
+
     $MaxNumberSpace = ($MaxNumber).ToString('N0')
     $MaxNumberWidth = $MaxNumberSpace.Length
 
@@ -30,7 +30,7 @@ if ((Test-Path variable:script:WriteCounts) -and $Script:WriteCounts.Count -gt 0
         }
         }
     }
-}   
+}
 
 Format-Humanize $scriptTimer
 
@@ -39,10 +39,10 @@ $secondsRan  = $elapsedTime.TotalSeconds
 Log-Line "Stopping Normally after $secondsRan Second(s)"
 
 End-BatchRunSessionTaskEntry
-      
+
 } catch {
     Show-Error "Untrapped exception in footer" -exitcode $_EXITCODE_UNTRAPPED_EXCEPTION
-}                                            
+}
 
 try {Stop-Transcript}catch{}
 
